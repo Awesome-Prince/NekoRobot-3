eac8a8aed94c4672f64799617138b89645fd50cf
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2022-01-07 12:01:47 +0530

    | Yūki • Black Knights Union Project | NekoRobot

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m NekoRobot
+web: python3 -m NekoRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE NekoRobot
+:: Enables virtual env mode and then starts Neko
+env\scripts\activate.bat && py -m NekoRobot
