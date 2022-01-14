7d804d81d84c39dd8b89d7de522ea85269427049
Lovely Prince <90893568+Hodacka@users.noreply.github.com>
2022-01-14 14:12:15 +0530

    Update forceSubscribe_sql.py

--- NekoRobot/modules/sql_extended/forceSubscribe_sql.py
+++ NekoRobot/modules/sql_extended/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from mrjoker.modules.sql import BASE, SESSION
+from NekoRobot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
