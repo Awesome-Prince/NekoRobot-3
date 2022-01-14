f5a8d81212ab34682791870e7e28667f79ae4a76
Lovely Prince <90893568+Hodacka@users.noreply.github.com>
2022-01-14 15:58:34 +0530

    Update memes.py

--- NekoRobot/modules/memes.py
+++ NekoRobot/modules/memes.py
@@ -9,8 +9,8 @@ from PIL import Image
 from spongemock import spongemock
 from telegram.ext import run_async
 
-from mrjoker import dispatcher
-from mrjoker.modules.disable import DisableAbleCommandHandler
+from NekoRobot import dispatcher
+from NekoRobot.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
