1620a3fe48d79359359971a0e8d39a35bb9e3d8c
Lovely Prince <90893568+Hodacka@users.noreply.github.com>
2022-01-14 16:07:51 +0530

    Update pluginshelper.py

--- NekoRobot/pyro/pluginshelper.py
+++ NekoRobot/pyro/pluginshelper.py
@@ -12,8 +12,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from mrjoker import OWNER_ID, SUPPORT_CHAT
-from mrjoker import pbot
+from NekoRobot import OWNER_ID, SUPPORT_CHAT
+from NekoRobot import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
