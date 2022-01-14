9dd84514a2bacf6a62ee0a5a45e1a13854340e96
Lovely Prince <90893568+Hodacka@users.noreply.github.com>
2022-01-14 15:55:14 +0530

    Update logchannel.py

--- NekoRobot/modules/logchannel.py
+++ NekoRobot/modules/logchannel.py
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from mrjoker import EVENT_LOGS, LOGGER, dispatcher
-    from mrjoker.modules.helper_funcs.chat_status import user_admin
-    from mrjoker.modules.sql import log_channel_sql as sql
+    from NekoRobot import EVENT_LOGS, LOGGER, dispatcher
+    from NekoRobot.modules.helper_funcs.chat_status import user_admin
+    from NekoRobot.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)
