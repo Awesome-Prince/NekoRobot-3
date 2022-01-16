1e453b4e8d3e7d6efe76dc25c6ff924c08ed7972
Lovely Prince <90893568+Hodacka@users.noreply.github.com>
2022-01-17 02:24:43 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -697,7 +697,6 @@ def main():
     # dispatcher.add_handler(test_handler)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
-    dispatcher.add_handispatcher.add_error_handler(error_callback)dler(settings_handler)
     dispatcher.add_handler(about_callback_handler)
     dispatcher.add_handler(help_callback_handler)
     dispatcher.add_handler(settings_callback_handler)
