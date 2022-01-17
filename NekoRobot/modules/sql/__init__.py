2dcbdea291b42fa255b6073dba7cb95da3936919
Lovely Prince <90893568+Hodacka@users.noreply.github.com>
2022-01-17 13:41:06 +0530

    Update __init__.py

--- NekoRobot/modules/sql/__init__.py
+++ NekoRobot/modules/sql/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from tg_bot import DB_URI, KInit, log
+from NekoRobot import DB_URI, KInit, log
 
 
 def start() -> scoped_session:
