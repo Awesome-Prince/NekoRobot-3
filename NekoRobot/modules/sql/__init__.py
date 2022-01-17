844ca582f7fab5164bc83609f30d1b7a0bb07350
Lovely Prince <90893568+Hodacka@users.noreply.github.com>
2022-01-17 13:39:56 +0530

    Add files via upload

---
+++ NekoRobot/modules/sql/__init__.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from tg_bot import DB_URI, KInit, log
+
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI, client_encoding="utf8", echo=KInit.DEBUG)
+    log.info("[PostgreSQL] Connecting to database......")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+try:
+    SESSION: scoped_session = start()
+except Exception as e:
+    log.exception(f'[PostgreSQL] Failed to connect due to {e}')
+    exit()
+   
+log.info("[PostgreSQL] Connection successful, session started.")
