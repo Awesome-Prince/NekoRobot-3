3567caf55f656319e4625692b5d89c95e7efd70f
Lovely Prince <90893568+Hodacka@users.noreply.github.com>
2022-01-19 14:16:09 +0530

    Update approve_sql.py

--- NekoRobot/modules/sql/approve_sql.py
+++ NekoRobot/modules/sql/approve_sql.py
@@ -31,7 +31,7 @@ from NekoRobot.modules.sql import BASE, SESSION
 class Approvals(BASE):
     __tablename__ = "approval"
     chat_id = Column(String(14), primary_key=True)
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
 
     def __init__(self, chat_id, user_id):
         self.chat_id = str(chat_id)  # ensure string
