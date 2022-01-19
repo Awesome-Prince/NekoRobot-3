54005dafe8c83062ec6fd90d0d47b1535251a498
Lovely Prince <90893568+Hodacka@users.noreply.github.com>
2022-01-19 21:42:31 +0530

    Update __init__.py

--- NekoRobot/modules/sql/__init__.py
+++ NekoRobot/modules/sql/__init__.py
@@ -21,18 +21,26 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from NekoRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from NekoRobot import DB_URI, KInit, log
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine(DB_URI, client_encoding="utf8", echo=KInit.DEBUG)
+    log.info("[PostgreSQL] Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=True))
 
 
 BASE = declarative_base()
-SESSION = start()
+try:
+    SESSION: scoped_session = start()
+except Exception as e:
+    log.exception(f'[PostgreSQL] Failed to connect due to {e}')
+    exit()
+   
+log.info("[PostgreSQL] Connection successful, session started.")
