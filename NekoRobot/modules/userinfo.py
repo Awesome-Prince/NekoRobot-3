ce18046e397afaca496dae0df53a1515f0ea9c87
Lovely Prince <90893568+Hodacka@users.noreply.github.com>
2022-01-20 15:07:56 +0530

    Update userinfo.py

--- NekoRobot/modules/userinfo.py
+++ NekoRobot/modules/userinfo.py
@@ -476,7 +476,7 @@ def stats(update, context):
             + "\n*Bot statistics*:\n"
             + "\n".join([mod.__stats__() for mod in STATS])
             + f"\n\n[✦ Support](https://t.me/{SUPPORT_CHAT}) | [✦ Updates](https://t.me/Black_Knights_Union)\n\n"
-            + "\n╘══「 by [Lovely Prince](https://t.me/Horimaya) 」\n",
+            + "\n╘══「 by [Lovely Prince](https://t.me/Awesome_Prince) 」\n",
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
                 [
@@ -498,7 +498,7 @@ def stats(update, context):
                     )
                     + f"\n\n✦ [Support](https://t.me/{SUPPORT_CHAT}) | ✦ [Updates](https://t.me/Black_Knights_Union)\n\n"
                 )
-                + "╘══「 by [Lovely Prince](https://t.me/Horimaya) 」\n"
+                + "╘══「 by [Lovely Prince](https://t.me/Awesome_Prince) 」\n"
             ),
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
