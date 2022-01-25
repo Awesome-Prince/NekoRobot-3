263614b3cefb55034e3c895aa031a7a954553856
Lovely Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-01-25 13:22:12 +0530

    Update misc.py

--- NekoRobot/modules/helper_funcs/misc.py
+++ NekoRobot/modules/helper_funcs/misc.py
@@ -96,12 +96,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⫷", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Close", callback_data="cutiepii_back"),
-             EqInlineKeyboardButton("⫸", callback_data="{}_prev({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("«", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("Close", callback_data="neko_back"),
+             EqInlineKeyboardButton("»", callback_data="{}_prev({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Close", callback_data="cutiepii_back")]]
+        pairs +=[[EqInlineKeyboardButton("Close", callback_data="neko_back")]]
 
     return pairs
 
