14601e966b45f5e3b41585bc279af3bf510c4113
Lovely Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-01-25 11:57:16 +0530

    Update wish.py

--- NekoRobot/modules/wish.py
+++ NekoRobot/modules/wish.py
@@ -8,9 +8,9 @@ async def wish(e):
  if e.is_reply:
          mm = random.randint(1,100)
          lol = await e.get_reply_message()
-         fire = "https://telegra.ph/file/82decd7a6cd88bd964f92.jpg"
-         await miku.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=lol)
+         fire = "https://telegra.ph/file/3fe0f7dedb81528a57313.jpg"
+         await neko.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.💜**\n\n__chance of success {mm}%__", reply_to=lol)
  if not e.is_reply:
          mm = random.randint(1,100)
-         fire = "https://telegra.ph/file/82decd7a6cd88bd964f92.jpg"
-         await miku.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=e)
+         fire = "https://telegra.ph/file/3fe0f7dedb81528a57313.jpg"
+         await neko.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your wish has been cast.💜**\n\n__chance of success {mm}%__", reply_to=e)
