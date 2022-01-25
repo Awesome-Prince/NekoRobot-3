d14d52cb9cf8ae104b2df10d27f2f06f29e8ef3b
Lovely Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-01-25 12:11:22 +0530

    Create guess.py

---
+++ NekoRobot/modules/guess.py
@@ -0,0 +1,16 @@
+import random
+from PIL import Image
+from NekoRobot import telethn as neko
+from telethon import events
+@neko.on(events.NewMessage(pattern="/guess ?(.*)"))
+async def wish(e):
+
+ if e.is_reply:
+         mm = random.randint(1,100)
+         lol = await e.get_reply_message()
+         fire = "https://telegra.ph/file/3fe0f7dedb81528a57313.jpg"
+         await neko.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your Guess is {mm}%__**\n\n__Correct!!!", reply_to=lol)
+ if not e.is_reply:
+         mm = random.randint(1,100)
+         fire = "https://telegra.ph/file/3fe0f7dedb81528a57313.jpg"
+         await neko.send_file(e.chat_id, fire,caption=f"**Hey [{e.sender.first_name}](tg://user?id={e.sender.id}), Your Guess has been cast.💜**\n\n__chance of success {mm}%__", reply_to=e)
