118faadd77fe70442d4783e7a05a95f8708972d7
Lovely Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-02-04 20:20:09 +0530

    Update NekoFunBot.py

--- NekoRobot/modules/NekoFunBot.py
+++ NekoRobot/modules/NekoFunBot.py
@@ -2,4 +2,4 @@ from .. events import register
 
 @register(pattern="^(?i)neko")
 async def start(event):
-    await event.send_message(event.chat_id, "Hello I'm Neko"
+    await event.send_message(event.chat_id, "Hello I'm Neko")
