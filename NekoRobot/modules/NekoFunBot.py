6a79414952acae19ba3fe4429e0c2c1673aea0f1
Lovely Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-02-05 00:14:30 +0530

    Update NekoFunBot.py

--- NekoRobot/modules/NekoFunBot.py
+++ NekoRobot/modules/NekoFunBot.py
@@ -1,5 +1,28 @@
-from .. events import register
+import asyncio
+import random
+import time
+import pbot as app
+from sys import version as pyver
+from typing import Dict, List, Union
 
-@register(pattern="^(?i)neko")
-async def start(event):
-    await event.send_message(event.chat_id, "Hello I'm Neko")
+import psutil
+from pyrogram import filters
+from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
+                            InlineKeyboardMarkup, Message)
+
+__MODULE__ = "FunBot"
+__HELP__ = """
+neko 
+- check neko is online.
+"""
+
+@app.on_callback_query(filters.regex("neko"))
+async def okaybhai(_, CallbackQuery):
+    await CallbackQuery.answer("What's up!")
+    out = start_pannel()
+    await CallbackQuery.edit_message_text(
+        text="Mewo (=^･ｪ･^=)",
+        reply_markup=InlineKeyboardMarkup(out[1]),
+    )
+
+
