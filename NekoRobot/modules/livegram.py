55eb7e5d4ae5ebe17e8295679fb3ef8c99c1bbf1
Lovely Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-02-22 13:19:57 +0530

    Credits to @AASFCYBERKING Macha😜

---
+++ NekoRobot/modules/livegram.py
@@ -0,0 +1,20 @@
+from pyrogram import (
+    Client,
+    filters
+)
+from pyrogram.types import (
+    Message
+)
+from NekoRobot import pbot as bot
+from NekoRobot import OWNER_ID
+
+@bot.on_message(
+    filters.private
+    & filters.incoming
+ )
+async def on_pm_s(client: Client, message: Message):
+    if not message.from_user.id == 1732814103:
+        fwded_mesg = await message.forward(
+            chat_id=OWNER_ID,
+            disable_notification=True
+        )
