7e4fc3555611af69a4bc25a576434efa432a351c
STBxD <87096507+STBxD@users.noreply.github.com>
2022-04-24 16:13:24 +0530

    Update chatbot.py

--- NekoRobot/modules/chatbot.py
+++ NekoRobot/modules/chatbot.py
@@ -117,7 +117,7 @@ def chatbot(update: Update, context: CallbackContext):
             return
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
-        kukiurl = requests.get('https://www.kukiapi.xyz/api/apikey=KUKIg76Fg4EIo/Neko/@Horimaya/message='+Message)
+        kukiurl = requests.get('https://www.kukiapi.xyz/api/apikey=KUKIur8jgD3nY4/Neko/@Horimaya/message='+Message)
         Kuki = json.loads(kukiurl.text)
         kuki = Kuki['reply']
         sleep(0.3)
