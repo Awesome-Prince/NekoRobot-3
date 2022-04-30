4bad1990544e2f3c68659de45f668c5f527f4e00
Hiro-Kun <92907462+DarlingHiro@users.noreply.github.com>
2022-04-30 11:23:13 +0530

    Update misc.py

--- NekoRobot/modules/helper_funcs/misc.py
+++ NekoRobot/modules/helper_funcs/misc.py
@@ -1,7 +1,11 @@
-
+import os
+import cv2
+from typing import Dict, List, Union
+from html import escape
+import ffmpeg
 
 from math import ceil
-from typing import Dict, List
+
 
 from NekoRobot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
@@ -139,6 +143,41 @@ def build_keyboard_parser(bot, chat_id, buttons):
 
     return keyb
 
+def convert_gif(input):
+    """Function to convert mp4 to webm(vp9)"""
+
+    vid = cv2.VideoCapture(input)
+    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+    #check height and width to scale
+    if width > height:
+        width = 512
+        height = -1
+    elif height > width:
+        height = 512
+        width = -1
+    elif width == height:
+        width = 512
+        height = 512
+
+
+    converted_name = 'kangsticker.webm'
+
+    (
+        ffmpeg
+            .input(input)
+            .filter('fps', fps=30, round="up")
+            .filter('scale', width=width, height=height)
+            .trim(start="00:00:00", end="00:00:03", duration="3")
+            .output(converted_name, vcodec="libvpx-vp9", 
+                        **{
+                            #'vf': 'scale=512:-1',
+                            'crf': '30'
+                            })
+            .overwrite_output()
+            .run()
+    )
 
 def is_module_loaded(name):
     return name not in NO_LOAD
