3cc9588dc1945bec989826ecfb6aaabed0cd0a2e
Lovely Boy 2 <92840649+lovelygf@users.noreply.github.com>
2022-05-21 22:02:00 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -77,17 +77,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_PIC = "https://telegra.ph/file/bdc771a084e2c806ec756.jpg"
 
-PM_START_TEXT = """
-────「 [{}] 」────
-*やあ! {} My Darling, Iam your SweetHeart💜,
-I am an Anime themed advance group management bot with a lot of Sexy Features.*
-➖➖➖➖➖➖➖➖➖➖➖➖➖
-◈ *Uptime:* `{}`
-◈ `{}` *users, across* `{}` *chats.*
-◈ *Thanks To @NekoXRobot For NekoRobot Repo*
-➖➖➖➖➖➖➖➖➖➖➖➖➖
-➢ Try The Help Buttons Below To Know My Abilities ♡
-"""
+
 
 buttons = [
     [
