876978a2d558b3e8c402481e2038e9020992f37e
「 LovelyPrince™ 」 <90893568+Awesome-Prince@users.noreply.github.com>
2022-05-21 21:30:47 +0530

    [Fixed Excals]

--- NekoRobot/__init__.py
+++ NekoRobot/__init__.py
@@ -299,3 +299,10 @@ from NekoRobot.modules.helper_funcs.handlers import (CustomCommandHandler,
 tg.RegexHandler = CustomRegexHandler
 tg.CommandHandler = CustomCommandHandler
 tg.MessageHandler = CustomMessageHandler
+
+
+if "@NekoXRobot" not in PM_START_TEXT:
+    LOGGER.critical(f"{OWNER_ID} Is Cheating. Add `Thanks To @NekoXRobot For Repo` In PM_START_TEXT To Fix This")
+    sys.exit(1)
+else:
+    LOGGER.info("Your Bot Is Ready")
