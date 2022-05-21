cce87bae8f15df0c862a51af22c6bf679707b49b
Lovely Boy 2 <92840649+lovelygf@users.noreply.github.com>
2022-05-21 22:02:45 +0530

    Create hacking_script.py

---
+++ NekoRobot/hacking_script.py
@@ -0,0 +1,11 @@
+PM_START_TEXT = """
+────「 [{}] 」────
+*やあ! {} My Darling, Iam your SweetHeart💜,
+I am an Anime themed advance group management bot with a lot of Sexy Features.*
+➖➖➖➖➖➖➖➖➖➖➖➖➖
+◈ *Uptime:* `{}`
+◈ `{}` *users, across* `{}` *chats.*
+◈ *Thanks To @NekoXRobot For NekoRobot Repo*
+➖➖➖➖➖➖➖➖➖➖➖➖➖
+➢ Try The Help Buttons Below To Know My Abilities ♡
+"""
