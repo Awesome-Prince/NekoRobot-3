5bb2846c416f0769c23bf638aad4d2787e96e410
「 LovelyPrince™ 」 <90893568+Awesome-Prince@users.noreply.github.com>
2022-05-23 22:49:09 +0530

    Update repo.py

--- NekoRobot/modules/repo.py
+++ NekoRobot/modules/repo.py
@@ -1,55 +1,47 @@
 """
-BSD 2-Clause License
-
+MIT License
 Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2021-2022, Awesome-Prince, <https://github.com/Awesome-Prince>
-Copyright (c) 2021-2022, Koyūki • Network, <https://github.com/Awesome-Prince/NekoRobot>
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Copyright (C) 2022 Awesome-Prince
+Copyright (c) 2022, Koyūki • Network, <https://github.com/Awesome-Prince/NekoRobot-3>
+This file is part of @NekoXRobot (Telegram Bot)
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the Software), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
-import github
 
-from pyrogram import filters
-from NekoRobot import pgram
-
-
-@pgram.on_message(filters.command("repo") & ~filters.edited)
-async def give_repo(c, m):
-    g = github.Github()
-    repo = g.get_repo("Awesome-Prince/NekoRobot-3")
-    list_of_users = "".join(
-        f"*{count}.* [{i.login}](https://github.com/{i.login})\n"
-        for count, i in enumerate(repo.get_contributors(), start=1)
-    )
-
-    text = f"""[Github](https://github.com/Awesome-Prince/NekoRobot-3) | [support group](https://t.me/Koyuki_Support)
-```----------------
-| Contributors |
-----------------```
-{list_of_users}"""
-    await m.reply(text, disable_web_page_preview=False)
-
-
-__mod_name__ = "Contributers"
-
+from telethon import events, Button, custom
+import re, os
+from NekoRobot.events import register
+from NekoRobot import telethn as tbot
+from NekoRobot import telethn as tgbot
+PHOTO = ""
+@register(pattern=("/alive"))
+async def awake(event):
+  NEKO = """
+         We Are So Happy To Announce That We Have Public Our NekoRobot Repo. ✨🥀
+➖➖➖➖➖➖➖➖➖➖➖➖➖
+「@NekoXRobot」
+➖➖➖➖➖➖➖➖➖➖➖➖➖
+Here is the Repo Deploy your Own NekoRobot.
+⚜️Repo ➛ https://github.com/Awesome-Prince/NekoRobot-3.git
+➖➖➖➖➖➖➖➖➖➖➖➖➖
+🔰 Thanks for your support 
+It's Fully stable Repo so you can deploy and make own Bot.
+──────────────────
+Powered By:- @Koyuki_Network
+"""
+  
+  BUTTON = [[Button.url("📢 Repository", "https://github.com/Awesome-Prince/NekoRobot-3"), Button.url("💻 Collaborators", "https://telegra.ph/Neko-X-05-23")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=NEKO,  buttons=BUTTON)
