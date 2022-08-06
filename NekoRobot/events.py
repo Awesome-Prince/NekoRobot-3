37163eaebaad3cfec441650041ba70ae5506b126
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-06 16:17:05 +0530

    Testing

--- NekoRobot/events.py
+++ NekoRobot/events.py
@@ -1,7 +1,7 @@
 """
 MIT License
 Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2022 Hodacka
+Copyright (C) 2022 Awesome-Prince
 Copyright (c) 2022, Yūki • Black Knights Union, <https://github.com/Hodacka/NekoRobot-3>
 This file is part of @NekoXRobot (Telegram Bot)
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -22,7 +22,7 @@ SOFTWARE.
 """
 
 from telethon import events
-
+import pyrogram
 from NekoRobot import telethn
 
 
