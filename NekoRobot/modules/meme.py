5cc7549296f0178754fb5fc7d65de4a2ecd2b7b5
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-08-08 14:46:07 +0000

    Auto Code Formatting

--- NekoRobot/modules/meme.py
+++ NekoRobot/modules/meme.py
@@ -3,6 +3,7 @@ import random
 import time
 
 import requests
+from NekoXRobot.events import register
 from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, run_async
@@ -14,7 +15,6 @@ from NekoRobot.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from NekoXRobot.events import register
 from NekoRobot.modules.helper_funcs.alternate import typing_action
 from NekoRobot.modules.helper_funcs.chat_status import is_user_admin
 from NekoRobot.modules.helper_funcs.extraction import extract_user
