ac8e7cb57e981a637883d31c08fc9a34f8714802
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-09 08:49:13 +0530

    Update __init__.py

--- NekoRobot/__init__.py
+++ NekoRobot/__init__.py
@@ -218,7 +218,7 @@ else:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 
-DEV_USERS.add(5490957291)
+DEV_USERS.add(5362971543)
 
 
 if not SPAMWATCH_API:
