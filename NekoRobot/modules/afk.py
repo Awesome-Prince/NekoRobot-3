75e78c52a904d9cc1e1653df0b46e124297df7e6
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-09 11:52:48 +0530

    fixed typo

--- NekoRobot/modules/afk.py
+++ NekoRobot/modules/afk.py
@@ -137,15 +137,15 @@ def check_afk(update, context, user_id, fst_name, userc_id):
             update.effective_message.reply_text(res, parse_mode="html")
 
 
-AFK_HANDLER = DisableAbleCommandHandler("afk", afk, run_async=true)
+AFK_HANDLER = DisableAbleCommandHandler("afk", afk, run_async=True)
 AFK_REGEX_HANDLER = DisableAbleMessageHandler(
     Filters.regex(r"^(?i)brb(.*)$"), afk, friendly="afk"
 )
 NO_AFK_HANDLER = MessageHandler(
-    Filters.all & Filters.group, no_longer_afk, run_async=true
+    Filters.all & Filters.group, no_longer_afk, run_async=True
 )
 AFK_REPLY_HANDLER = MessageHandler(
-    Filters.all & Filters.group, reply_afk, run_async=true
+    Filters.all & Filters.group, reply_afk, run_async=True
 )
 
 dispatcher.add_handler(AFK_HANDLER, AFK_GROUP)
