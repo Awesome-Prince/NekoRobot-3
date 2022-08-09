b5c9015641cc6270cd6198b426c5a69ebee0966a
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-09 15:31:15 +0530

    Update ud.py

--- NekoRobot/modules/ud.py
+++ NekoRobot/modules/ud.py
@@ -1,24 +1,26 @@
-from requests import get
-from telegram import Bot, Update
-
+import requests
 from NekoRobot import dispatcher
 from NekoRobot.modules.disable import DisableAbleCommandHandler
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
 
 
-def ud(bot: Bot, update: Update):
+def ud(update: Update, context: CallbackContext):
     message = update.effective_message
     text = message.text[len("/ud ") :]
-    results = get(f"http://api.urbandictionary.com/v0/define?term={text}").json()
-    reply_text = f'Word: {text}\nDefinition: {results["list"][0]["definition"]}'
-    message.reply_text(reply_text)
+    results = requests.get(
+        f"https://api.urbandictionary.com/v0/define?term={text}",
+    ).json()
+    try:
+        reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
+    except:
+        reply_text = "No results found."
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 
-__help__ = """
- 🔹 `/ud:{word}` Type the word or expression you want to search use. like `/ud` telegram Word: Telegram Definition: A once-popular system of telecommunications, in which the sender would contact the telegram service and speak their [message] over the [phone]. The person taking the message would then send it, via a teletype machine, to a telegram office near the receiver's [address]. The message would then be hand-delivered to the addressee. From 1851 until it discontinued the service in 2006, Western Union was the best-known telegram service in the world.
-"""
+UD_HANDLER = DisableAbleCommandHandler(["ud"], ud, run_async=True)
 
-__mod_name__ = "Urban dictionary"
+dispatcher.add_handler(UD_HANDLER)
 
-ud_handle = DisableAbleCommandHandler("ud", ud, run_async=True)
-
-dispatcher.add_handler(ud_handle)
+__command_list__ = ["ud"]
+__handlers__ = [UD_HANDLER]
