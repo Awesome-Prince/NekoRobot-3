4194d225e0e1565fa512da005258834ec42c89bd
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-09 12:07:28 +0530

    Update stickers.py

--- NekoRobot/modules/stickers.py
+++ NekoRobot/modules/stickers.py
@@ -606,7 +606,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "ariel.ttf"
     else:
-        fnt = "./KURUMIBOT/resources/Vampire Wars.otf"
+        fnt = "./NekoRobot/LOGO_FONT/Vampire Wars.otf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
