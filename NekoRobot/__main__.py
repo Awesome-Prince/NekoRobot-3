f1ad8bea5d51c0e05243114864b27786e60afc6d
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-09 11:13:08 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -18,7 +18,7 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE......
+SOFTWARE.
 """
 
 import importlib
@@ -667,7 +667,6 @@ def get_settings(update: Update, context: CallbackContext):
                             InlineKeyboardButton(
                                 text="Settings",
                                 url=f"t.me/{BOT_USERNAME}?start=stngs_{}"
-                                ),
                             )
                         ]
                     ]
