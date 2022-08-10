4837bf7b97042d0a1b2aea81ac60c2fd77004a48
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-10 19:03:33 +0530

    lmao

---
+++ NekoRobot/modules/crypto.py
@@ -0,0 +1,70 @@
+import requests
+
+from NekoRobot import pgram
+from NekoRobot.utils.errors import capture_err
+from pyrogram import filters
+
+
+def ikb(data: dict, row_width: int = 2):
+    """
+    Converts a dict to pyrogram buttons
+    Ex: dict_to_keyboard({"click here": "this is callback data"})
+    """
+    return keyboard(data.items(), row_width=row_width)
+
+
+n = "\n"
+w = " "
+
+bold = lambda x: f"**{x}:** "
+bold_ul = lambda x: f"**--{x}:**-- "
+
+mono = lambda x: f"`{x}`{n}"
+
+
+def section(
+    title: str,
+    body: dict,
+    indent: int = 2,
+    underline: bool = False,
+) -> str:
+
+    text = (bold_ul(title) + n) if underline else bold(title) + n
+
+    for key, value in body.items():
+        text += (indent * w + bold(key) +
+                 ((value[0] + n) if isinstance(value, list) else mono(value)))
+    return text
+
+
+@pgram.on_message(filters.command("crypto"))
+@capture_err
+async def crypto(_, message):
+    if len(message.command) < 2:
+        return await message.reply("/crypto `[currency]`")
+
+    currency = message.text.split(None, 1)[1].lower()
+
+    btn = ikb(
+        {"Available Currencies": "https://plotcryptoprice.herokuapp.com"}, )
+
+    m = await message.reply("`Processing...`")
+
+    try:
+        r = await requests.get(
+            "https://x.wazirx.com/wazirx-falcon/api/v2.0/crypto_rates",
+            timeout=5,
+        )
+    except Exception:
+        return await m.edit("[ERROR]: Something went wrong.")
+
+    if currency not in r:
+        return await m.edit(
+            "[ERROR]: INVALID CURRENCY",
+            reply_markup=btn,
+        )
+
+    body = {i.upper(): j for i, j in r.get(currency).items()}
+
+    text = section(f"Current Crypto Rates For {currency.upper()}", body)
+    await m.edit(text, reply_markup=btn)
