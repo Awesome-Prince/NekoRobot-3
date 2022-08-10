c57f45a07fe4e2390df0cd305d27b526b81ef1dc
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-10 19:43:52 +0530

    Update userinfo_sql.py

--- NekoRobot/modules/sql/userinfo_sql.py
+++ NekoRobot/modules/sql/userinfo_sql.py
@@ -1,37 +1,12 @@
-"""
-MIT License
-Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2022 Awesome-Prince
-Copyright (c) 2022, BlackLover  •  Network, <https://github.com/Awesome-Prince/NekoRobot-3>
-This file is part of @NekoXRobot (Telegram Bot)
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the Software), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import threading
 
-from sqlalchemy import Column, UnicodeText
-from sqlalchemy.sql.sqltypes import BigInteger
-
 from NekoRobot.modules.sql import BASE, SESSION
+from sqlalchemy import Column, Integer, UnicodeText
 
 
 class UserInfo(BASE):
     __tablename__ = "userinfo"
-    user_id = Column(BigInteger, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
     info = Column(UnicodeText)
 
     def __init__(self, user_id, info):
@@ -44,7 +19,7 @@ class UserInfo(BASE):
 
 class UserBio(BASE):
     __tablename__ = "userbio"
-    user_id = Column(BigInteger, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
     bio = Column(UnicodeText)
 
     def __init__(self, user_id, bio):
