9918300adbe4ffff30ddb5ea85962a7317fdfd9a
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-10 18:39:45 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop NekoRobot
-net start NekoRobot
+
+nssm restart NekoRobot
