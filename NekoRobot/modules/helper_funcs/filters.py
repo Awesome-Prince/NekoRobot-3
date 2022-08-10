4982cc11f8e4e10df350d32e376b44d65f90836b
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-10 19:07:41 +0530

    Update filters.py

--- NekoRobot/modules/helper_funcs/filters.py
+++ NekoRobot/modules/helper_funcs/filters.py
@@ -28,12 +28,50 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-
+from NekoRobot import DEV_USERS, DRAGONS, DEMONS
 from telegram import Message
-from telegram.ext.filters import MessageFilter
+from telegram.ext import MessageFilter
 
-    class _IsAnonChannel(MessageFilter):
+
+class CustomFilters:
+    class _Supporters(MessageFilter):
         def filter(self, message: Message):
-            return bool((message.from_user and message.from_user.id == 136817688 ))
+            return bool(message.from_user and message.from_user.id in DEMONS)
 
-    is_anon_channel = _IsAnonChannel()
+    support_filter = _Supporters()
+
+    class _Sudoers(MessageFilter):
+        def filter(self, message: Message):
+            return bool(message.from_user and message.from_user.id in DRAGONS)
+
+    sudo_filter = _Sudoers()
+
+    class _Developers(MessageFilter):
+        def filter(self, message: Message):
+            return bool(message.from_user and message.from_user.id in DEV_USERS)
+
+    dev_filter = _Developers()
+
+    class _MimeType(MessageFilter):
+        def __init__(self, mimetype):
+            self.mime_type = mimetype
+            self.name = "CustomFilters.mime_type({})".format(self.mime_type)
+
+        def filter(self, message: Message):
+            return bool(
+                message.document and message.document.mime_type == self.mime_type,
+            )
+
+    mime_type = _MimeType
+
+    class _HasText(MessageFilter):
+        def filter(self, message: Message):
+            return bool(
+                message.text
+                or message.sticker
+                or message.photo
+                or message.document
+                or message.video,
+            )
+
+    has_text = _HasText()
