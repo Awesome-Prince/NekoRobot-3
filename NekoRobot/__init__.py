c744ace72ec5f01b0887d727aff35b929ccc5038
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-08-11 12:16:42 +0000

    Initial commit V13.12

--- NekoRobot/__init__.py
+++ NekoRobot/__init__.py
@@ -219,7 +219,9 @@ pgram = Client(
 )
 
 # Credits Logger
-print("[NEKOROBOT] NEKO Is Starting. | BlackLover • Network Project | BSD 2-Clause License.")
+print(
+    "[NEKOROBOT] NEKO Is Starting. | BlackLover • Network Project | BSD 2-Clause License."
+)
 print(
     "[NEKOROBOT] Mewo Mewo! Successfully Connected With BlackLover • Data Center • Chennai"
 )
