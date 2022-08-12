b1191c2867b48ee6067a939e04ace37aa343c3d3
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-12 19:14:45 +0530

    new style

--- NekoRobot/modules/bans.py
+++ NekoRobot/modules/bans.py
@@ -546,21 +546,21 @@ __mod_name__ = "Bans/Mutes"
 
 __help__ = """
 *User Commands:*
-  ➢ `/kickme`*:* kicks the user who issued the command
+  ➛ `/kickme`*:* kicks the user who issued the command
   
 *Admins only:*
-  ➢ `/ban <userhandle>`*:* bans a user. (via handle, or reply)
+  ➛ `/ban <userhandle>`*:* bans a user. (via handle, or reply)
   banme
-  ➢ `/sban <userhandle>`*:* Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
-  ➢ `/tban <userhandle> x(m/h/d)`*:* bans a user for `x` time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
-  ➢ `/unban <userhandle>`*:* unbans a user. (via handle, or reply)
-  ➢ `/kick <userhandle>`*:* kicks a user out of the group, (via handle, or reply)
-  ➢ `/mute <userhandle>`*:* silences a user. Can also be used as a reply, muting the replied to user.
-  ➢ `/tmute <userhandle> x(m/h/d)`*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
-  ➢ `/unmute <userhandle>`*:* unmutes a user. Can also be used as a reply, muting the replied to user.
-  ➢ `/zombies`*:* searches deleted accounts
-  ➢ `/zombies clean`*:* removes deleted accounts from the group.
-  ➢ `/snipe <chatid> <string>`*:* Make me send a message to a specific chat.
+  ➛ `/sban <userhandle>`*:* Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
+  ➛ `/tban <userhandle> x(m/h/d)`*:* bans a user for `x` time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
+  ➛ `/unban <userhandle>`*:* unbans a user. (via handle, or reply)
+  ➛ `/kick <userhandle>`*:* kicks a user out of the group, (via handle, or reply)
+  ➛ `/mute <userhandle>`*:* silences a user. Can also be used as a reply, muting the replied to user.
+  ➛ `/tmute <userhandle> x(m/h/d)`*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
+  ➛ `/unmute <userhandle>`*:* unmutes a user. Can also be used as a reply, muting the replied to user.
+  ➛ `/zombies`*:* searches deleted accounts
+  ➛ `/zombies clean`*:* removes deleted accounts from the group.
+  ➛ `/snipe <chatid> <string>`*:* Make me send a message to a specific chat.
 """
 
 
