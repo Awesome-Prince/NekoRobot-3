e89ecf083a74d1f2f6e10fc6cefb3215986d2bef
「 LovelyPrince™ 」 <harishk2appu@gmail.com>
2022-08-13 15:49:45 +0530

    new symbol

--- NekoRobot/modules/disasters.py
+++ NekoRobot/modules/disasters.py
@@ -554,86 +554,86 @@ Commands listed here only work for users with special access are mainly used for
 Group admins/group owners do not need these commands. 
 
  ╔ *List all special users:*
- ╠ `/aranks`*:* Lists all A Rank Hunters
- ╠ `/branks`*:* Lists all B Rank Hunters
- ╠ `/cranks`*:* Lists all C Rank Hunters
- ╠ `/dranks`*:* Lists all D Rank Hunters
- ╠ `/sranks`*:* Lists all S Rank Hunters
- ╠ `/addarank`*:* Adds a user to A Rank Hunter
- ╠ `/addbrank`*:* Adds a user to B Rank Hunter
- ╠ `/addcrank`*:* Adds a user to C Rank Hunter
- ╠ `/adddrank`*:* Adds a user to D Rank Hunter
+ ➤`/aranks`*:* Lists all A Rank Hunters
+ ➤`/branks`*:* Lists all B Rank Hunters
+ ➤`/cranks`*:* Lists all C Rank Hunters
+ ➤`/dranks`*:* Lists all D Rank Hunters
+ ➤`/sranks`*:* Lists all S Rank Hunters
+ ➤`/addarank`*:* Adds a user to A Rank Hunter
+ ➤`/addbrank`*:* Adds a user to B Rank Hunter
+ ➤`/addcrank`*:* Adds a user to C Rank Hunter
+ ➤`/adddrank`*:* Adds a user to D Rank Hunter
  ╚ `Add dev doesnt exist, devs should know how to add themselves`
 
  ╔ *Ping:*
- ╠ `/ping`*:* gets ping time of bot to telegram server
+ ➤`/ping`*:* gets ping time of bot to telegram server
  ╚ `/pingall`*:* gets all listed ping times
 
  ╔ *Broadcast: (Bot owner only)*
- ╠  *Note:* This supports basic markdown
- ╠ `/broadcastall`*:* Broadcasts everywhere
- ╠ `/broadcastusers`*:* Broadcasts too all users
+ ➤ *Note:* This supports basic markdown
+ ➤`/broadcastall`*:* Broadcasts everywhere
+ ➤`/broadcastusers`*:* Broadcasts too all users
  ╚ `/broadcastgroups`*:* Broadcasts too all groups
 
  ╔ *Groups Info:*
- ╠ `/groups`*:* List the groups with Name, ID, members count as a txt
- ╠ `/leave <ID>`*:* Leave the group, ID must have hyphen
- ╠ `/stats`*:* Shows overall bot stats
- ╠ `/getchats`*:* Gets a list of group names the user has been seen in. Bot owner only
+ ➤`/groups`*:* List the groups with Name, ID, members count as a txt
+ ➤`/leave <ID>`*:* Leave the group, ID must have hyphen
+ ➤`/stats`*:* Shows overall bot stats
+ ➤`/getchats`*:* Gets a list of group names the user has been seen in. Bot owner only
  ╚ `/ginfo username/link/ID`*:* Pulls info panel for entire group
 
  ╔ *Access control:* 
- ╠ `/ignore`*:* Blacklists a user from 
- ╠  using the bot entirely
- ╠ `/notice`*:* Removes user from blacklist
+ ➤`/ignore`*:* Blacklists a user from 
+ ➤ using the bot entirely
+ ➤`/notice`*:* Removes user from blacklist
  ╚ `/ignoredlist`*:* Lists ignored users
 
  ╔ *Module loading:*
- ╠ `/listmodules`*:* Prints modules and their names
- ╠ `/unload <name>`*:* Unloads module dynamically
+ ➤`/listmodules`*:* Prints modules and their names
+ ➤`/unload <name>`*:* Unloads module dynamically
  ╚ `/load <name>`*:* Loads module
 
  ╔ *Speedtest:*
  ╚ `/speedtest`*:* Runs a speedtest and gives you 2 options to choose from, text or image output
 
  ╔ *Global Bans:*
- ╠ `/gban user reason`*:* Globally bans a user
+ ➤`/gban user reason`*:* Globally bans a user
  ╚ `/ungban user reason`*:* Unbans the user from the global bans list
 
  ╔ *Module loading:*
- ╠ `/listmodules`*:* Lists names of all modules
- ╠ `/load modulename`*:* Loads the said module to 
- ╠   memory without restarting.
- ╠ `/unload modulename`*:* Loads the said module from
+ ➤`/listmodules`*:* Lists names of all modules
+ ➤`/load modulename`*:* Loads the said module to 
+ ➤  memory without restarting.
+ ➤`/unload modulename`*:* Loads the said module from
  ╚   memory without restarting.memory without restarting the bot 
 
  ╔ *Remote commands:*
- ╠ `/rban user group`*:* Remote ban
- ╠ `/runban user group`*:* Remote un-ban
- ╠ `/rpunch user group`*:* Remote punch
- ╠ `/rmute user group`*:* Remote mute
+ ➤`/rban user group`*:* Remote ban
+ ➤`/runban user group`*:* Remote un-ban
+ ➤`/rpunch user group`*:* Remote punch
+ ➤`/rmute user group`*:* Remote mute
  ╚ `/runmute user group`*:* Remote un-mute
 
  ╔ *Windows self hosted only:*
- ╠ `/reboot`*:* Restarts the bots service
+ ➤`/reboot`*:* Restarts the bots service
  ╚ `/gitpull`*:* Pulls the repo and then restarts the bots service
 
  ╔ *Chatbot:* 
  ╚ `/listaichats`*:* Lists the chats the chatmode is enabled in
  
  ╔ *Debugging and Shell:* 
- ╠ `/debug <on/off>`*:* Logs commands to updates.txt
- ╠ `/logs`*:* Run this in support group to get logs in pm
- ╠ `/eval`*:* Self explanatory
- ╠ `/sh`*:* Runs shell command
- ╠ `/shell`*:* Runs shell command
- ╠ `/clearlocals`*:* As the name goes
- ╠ `/dbcleanup`*:* Removes deleted accs and groups from db
+ ➤`/debug <on/off>`*:* Logs commands to updates.txt
+ ➤`/logs`*:* Run this in support group to get logs in pm
+ ➤`/eval`*:* Self explanatory
+ ➤`/sh`*:* Runs shell command
+ ➤`/shell`*:* Runs shell command
+ ➤`/clearlocals`*:* As the name goes
+ ➤`/dbcleanup`*:* Removes deleted accs and groups from db
  ╚ `/py`*:* Runs python code
  
  ╔ *Global Bans:*
- ╠ `/gban <id> <reason>`*:* Gbans the user, works by reply too
- ╠ `/ungban`*:* Ungbans the user, same usage as gban
+ ➤`/gban <id> <reason>`*:* Gbans the user, works by reply too
+ ➤`/ungban`*:* Ungbans the user, same usage as gban
  ╚ `/gbanlist`*:* Outputs a list of gbanned users
 
 Visit @{SUPPORT_CHAT} for more information.
