18f33b3d70bd2170fc46c24ffd1ca3344425c0d4
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-08-13 16:03:13 +0000

    Initial commit V13.12

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -447,10 +447,13 @@ async def neko_callback_data(update, context):
     elif query.data == "neko_back":
         first_name = update.effective_user.first_name
         await query.message.edit_text(
-            PM_START_TEXT.format(escape_markdown(context.bot.first_name),
-                                 escape_markdown(first_name),
-                                 escape_markdown(uptime), sql.num_users(),
-                                 sql.num_chats()),
+            PM_START_TEXT.format(
+                escape_markdown(context.bot.first_name),
+                escape_markdown(first_name),
+                escape_markdown(uptime),
+                sql.num_users(),
+                sql.num_chats(),
+            ),
             reply_markup=InlineKeyboardMarkup(buttons),
             parse_mode=ParseMode.MARKDOWN,
             timeout=60,
