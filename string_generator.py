2c5c094032a1cd27800c453315108d476b90dfb2
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-08-16 07:06:40 +0000

    Music Testing

--- string_generator.py
+++ string_generator.py
@@ -1,11 +1,12 @@
-from pyrogram.sync import TelegramClient
 from pyrogram.sessions import StringSession
+from pyrogram.sync import TelegramClient
 
-
-print("""Please go-to my.telegram.org
+print(
+    """Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
-Create a new application, by entering the required details""")
+Create a new application, by entering the required details"""
+)
 APP_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
