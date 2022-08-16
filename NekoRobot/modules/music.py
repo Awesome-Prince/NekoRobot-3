78e6dca960ff151624bbd3df233093a6f5ff728f
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-08-16 07:03:18 +0000

    Music Testing

--- NekoRobot/modules/music.py
+++ NekoRobot/modules/music.py
@@ -1,11 +1,12 @@
-from pyrogram import filters
-from NekoRobot import pbot, musicbot
 import pytgcalls
+from pyrogram import filters
+
+from NekoRobot import musicbot, pbot
 
 calls = pytgcalls.GroupCallFactory(musicbot).get_group_call()
 
 
-@pbot.on_message(filters.command('play'))
+@pbot.on_message(filters.command("play"))
 async def play(_, message):
     try:
         await musicbot.start()
@@ -13,14 +14,14 @@ async def play(_, message):
         pass
     reply = message.reply_to_message
     if reply:
-        fk = await message.reply('Downloading....')
+        fk = await message.reply("Downloading....")
         path = await reply.download()
         await calls.join(message.chat.id)
         await calls.start_audio(path, repeat=False)
-        await fk.edit('playing...')
+        await fk.edit("playing...")
 
 
-@pbot.on_message(filters.command('vplay'))
+@pbot.on_message(filters.command("vplay"))
 async def vplay(_, message):
     try:
         await musicbot.start()
@@ -31,15 +32,15 @@ async def vplay(_, message):
         path = await reply.download()
         await calls.join(message.chat.id)
         await calls.start_video(path, repeat=False)
-        await fk.edit('playing...')
+        await fk.edit("playing...")
 
 
-@pbot.on_message(filters.command('leavevc'))
+@pbot.on_message(filters.command("leavevc"))
 async def leavevc(_, message):
     await calls.stop()
     await calls.leave_current_group_call()
 
 
-@pbot.on_message(filters.command('pause'))
+@pbot.on_message(filters.command("pause"))
 async def pause(_, message):
     await calls.pause_stream()
