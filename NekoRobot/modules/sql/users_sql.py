ad4f8eda696b9068c5e76e10ec141edf4524d5e5
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-16 12:41:18 +0530

    Update users_sql.py

--- NekoRobot/modules/sql/users_sql.py
+++ NekoRobot/modules/sql/users_sql.py
@@ -1,5 +1,7 @@
 import threading
 
+from NekoRobot import dispatcher
+from NekoRobot.modules.sql import BASE, SESSION
 from sqlalchemy import (
     Column,
     ForeignKey,
@@ -9,14 +11,12 @@ from sqlalchemy import (
     UniqueConstraint,
     func,
 )
-
-from NekoRobot import dispatcher
-from NekoRobot.modules.sql import BASE, SESSION
+from sqlalchemy.sql.sqltypes import BigInteger
 
 
 class Users(BASE):
     __tablename__ = "users"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     username = Column(UnicodeText)
 
     def __init__(self, user_id, username=None):
@@ -42,7 +42,7 @@ class Chats(BASE):
 
 class ChatMembers(BASE):
     __tablename__ = "chat_members"
-    priv_chat_id = Column(Integer, primary_key=True)
+    priv_chat_id = Column(BigInteger, primary_key=True)
     # NOTE: Use dual primary key instead of private primary key?
     chat = Column(
         String(14),
@@ -50,7 +50,7 @@ class ChatMembers(BASE):
         nullable=False,
     )
     user = Column(
-        Integer,
+        BigInteger,
         ForeignKey("users.user_id", onupdate="CASCADE", ondelete="CASCADE"),
         nullable=False,
     )
