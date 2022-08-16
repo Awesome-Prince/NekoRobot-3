e44f101f6948e3497845b21712df22663046d7e1
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-16 16:26:36 +0530

    Update music.py

--- NekoRobot/modules/music.py
+++ NekoRobot/modules/music.py
@@ -1,46 +1,33 @@
-import pytgcalls
 from pyrogram import filters
+from NekoRobot import pbot , musicbot
+import pytgcalls
 
-from NekoRobot import musicbot, pbot
 
 calls = pytgcalls.GroupCallFactory(musicbot).get_group_call()
 
 
-@pbot.on_message(filters.command("play"))
-async def play(_, message):
-    try:
-        await musicbot.start()
-    except:
-        pass
-    reply = message.reply_to_message
-    if reply:
-        fk = await message.reply("Downloading....")
-        path = await reply.download()
-        await calls.join(message.chat.id)
-        await calls.start_audio(path, repeat=False)
-        await fk.edit("playing...")
+@pbot.on_message(filters.command('play'))
+async def play(_,message):
+  reply = message.reply_to_message
+  if reply:
+    fk = await message.reply('Downloading....')
+    path = await reply.download()
+    await calls.join(message.chat.id)
+    await calls.start_audio(path)
+    await fk.edit('playing...')
 
+    
+@pbot.on_message(filters.command('vplay'))
+async def vplay(_,message):
+  reply = message.reply_to_message
+  if reply:
+    path = reply.download()
+    await calls.join(message.chat.id)
+    await calls.start_video(path)
+    await fk.edit('playing...')
 
-@pbot.on_message(filters.command("vplay"))
-async def vplay(_, message):
-    try:
-        await musicbot.start()
-    except:
-        pass
-    reply = message.reply_to_message
-    if reply:
-        path = await reply.download()
-        await calls.join(message.chat.id)
-        await calls.start_video(path, repeat=False)
-        await fk.edit("playing...")
-
-
-@pbot.on_message(filters.command("leavevc"))
-async def leavevc(_, message):
-    await calls.stop()
-    await calls.leave_current_group_call()
-
-
-@pbot.on_message(filters.command("pause"))
-async def pause(_, message):
-    await calls.pause_stream()
+    
+@pbot.on_message(filters.command('leavevc'))
+def leavevc(_,message):
+  await group_call.stop()
+  await group_call.leave_current_group_call()
