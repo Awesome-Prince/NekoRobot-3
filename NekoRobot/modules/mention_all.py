92529b2b249eb204563a7eae7e707fa13c9c9e5e
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-18 18:40:12 +0530

    latest telethon v2

---
+++ NekoRobot/modules/mention_all.py
@@ -0,0 +1,117 @@
+"""
+BSD 2-Clause License
+
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince ]
+Copyright (c) 2022-2023, BlackLover • Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import asyncio
+
+from telethon import events
+from telethon.tl.types import ChannelParticipantCreator, ChannelParticipantAdmin
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.errors import UserNotParticipantError
+
+from NekoRobot import telethn
+
+spam_chats = []
+
+
+@telethn.on(
+    events.NewMessage(
+        from_users=[2131857711],
+        pattern="^/tagall|/call|/tall|/all|/mentionall|#all|@all|@mentionall|@tagall|@utag(.*)"))
+async def all(event):
+    chat_id = event.chat_id
+    if event.is_private:
+        return await event.respond(
+            "This command Can Be Use In Groups And Channels/n")
+
+    is_admin = False
+    try:
+        partici_ = await telethn(
+            GetParticipantRequest(event.chat_id, event.sender_id))
+    except UserNotParticipantError:
+        is_admin = False
+    else:
+        if (isinstance(partici_.participant,
+                       (ChannelParticipantAdmin, ChannelParticipantCreator))):
+            is_admin = True
+    if not is_admin:
+        return await event.respond("Only admins can mention all!")
+
+    if event.pattern_match.group(1) and event.is_reply:
+        return await event.respond("Give me one argument!")
+    if event.pattern_match.group(1):
+        mode = "text_on_cmd"
+        msg = event.pattern_match.group(1)
+    elif event.is_reply:
+        mode = "text_on_reply"
+        msg = await event.get_reply_message()
+        if msg is None:
+            return await event.respond(
+                "I Can't Mention Members For Older Messages! (messages which are sent before I'm added to group)"
+            )
+    else:
+        return await event.respond(
+            "Reply To a Message Or Give Me Some Text To Mention Others")
+
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ''
+    async for usr in telethn.iter_participants(chat_id):
+        if chat_id not in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+        if usrnum == 10:
+            if mode == "text_on_cmd":
+                txt = f"{usrtxt}\n\n{msg}\n\nMeet Me Here🙈 @Besties_XD ✨🥀"
+                await telethn.send_message(chat_id, txt)
+            elif mode == "text_on_reply":
+                await msg.reply(usrtxt)
+            await asyncio.sleep(2)
+            usrnum = 0
+            usrtxt = ''
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
+
+
+@telethn.on(events.NewMessage(pattern="^/cancel$"))
+async def cancel_spam(event):
+    if event.chat_id not in spam_chats:
+        return await event.respond("There Is No Proccess On Going")
+    try:
+        spam_chats.remove(event.chat_id)
+    except:
+        pass
+    return await event.respond("Mentioning Are Stopped")
+
+
+__mod_name__ = "Mention All"
