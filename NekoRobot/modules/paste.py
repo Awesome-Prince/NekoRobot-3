def9a29a34d4e73545c35134421076777eb41a60
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-18 18:21:52 +0530

    Push Pyrogram V20

--- NekoRobot/modules/paste.py
+++ NekoRobot/modules/paste.py
@@ -1,3 +1,34 @@
+"""
+BSD 2-Clause License
+
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince ]
+Copyright (c) 2022-2023, BlackLover • Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 import asyncio
 import os
 import re
@@ -7,20 +38,18 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton
 
-from NekoRobot import aiohttpsession as session
-from NekoRobot import pbot
+from NekoRobot import pgram, aiohttpsession
 from NekoRobot.utils.errors import capture_err
 from NekoRobot.utils.pastebin import paste
 
-__mod_name__ = "Paste"
-
 pattern = re.compile(r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$")
+Neko_PYRO_Paste = filters.command("paste")
 
 
 async def isPreviewUp(preview: str) -> bool:
     for _ in range(7):
         try:
-            async with session.head(preview, timeout=2) as resp:
+            async with aiohttpsession.head(preview, timeout=2) as resp:
                 status = resp.status
                 size = resp.content_length
         except asyncio.exceptions.TimeoutError:
@@ -28,11 +57,12 @@ async def isPreviewUp(preview: str) -> bool:
         if status == 404 or (status == 200 and size == 0):
             await asyncio.sleep(0.4)
         else:
-            return True if status == 200 else False
+            return status == 200
     return False
 
 
-@pbot.on_message(filters.command)("paste") & ~filters.edited)
+@pgram.on_message(Neko_PYRO_Paste)
+@pgram.on_edited_message(Neko_PYRO_Paste)
 @capture_err
 async def paste_func(_, message):
     if not message.reply_to_message:
@@ -51,14 +81,19 @@ async def paste_func(_, message):
             content = await f.read()
         os.remove(doc)
     link = await paste(content)
-    preview = link + "/preview.png"
+    preview = f"{link}/preview.png"
     button = InlineKeyboard(row_width=1)
     button.add(InlineKeyboardButton(text="Paste Link", url=link))
 
     if await isPreviewUp(preview):
         try:
-            await message.reply_photo(photo=preview, quote=False, reply_markup=button)
+            await message.reply_photo(photo=preview,
+                                      quote=False,
+                                      reply_markup=button)
             return await m.delete()
         except Exception:
             pass
     return await m.edit(link)
+
+
+__mod_name__ = "Paste"
