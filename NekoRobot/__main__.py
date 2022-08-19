d855b8e306e87d9bb67377368475fbbf8bb68820
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-19 15:05:04 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -694,15 +694,15 @@ async def migrate_chats(update: Update):
 
 
 def main():
-    test_handler = DisableAbleCommandHandler("test", test, run_async=True)
-    start_handler = DisableAbleCommandHandler("start", start, run_async=True)
+    test_handler = CommandHandler("test", test, run_async=True)
+    start_handler = CommandHandler("start", start, run_async=True)
 
-    help_handler = DisableAbleCommandHandler("help", get_help, run_async=True)
+    help_handler = CommandHandler("help", get_help, run_async=True)
     help_callback_handler = CallbackQueryHandler(
         help_button, pattern=r"help_.*", run_async=True
     )
 
-    settings_handler = DisableAbleCommandHandler("settings", get_settings)
+    settings_handler = CommandHandler("settings", get_settings)
     settings_callback_handler = CallbackQueryHandler(
         settings_button, pattern=r"stngs_", run_async=True
     )
@@ -710,7 +710,7 @@ def main():
     data_callback_handler = CallbackQueryHandler(
         neko_callback_data, pattern=r"neko_", run_async=True
     )
-    donate_handler = DisableAbleCommandHandler("donate", donate, run_async=True)
+    donate_handler = CommandHandler("donate", donate, run_async=True)
     migrate_handler = MessageHandler(
         Filters.status_update.migrate, migrate_chats, run_async=True
     )
