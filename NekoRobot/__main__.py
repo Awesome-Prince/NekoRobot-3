4de14f5086ab5b8acdb9ac45ddb799183dc4b808
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-19 16:15:32 +0530

    Removed Unwanted Lines

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -111,8 +111,7 @@ HELP_MSG = "Click the button below to get help manu in your pm."
 START_MSG = "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>"
 
 PM_START_TEXT = """
-────「 [{}](https://telegra.ph/file/bdc771a084e2c806ec756.jpg)) 」────
-
+────「 [{}](https://telegra.ph/file/bdc771a084e2c806ec756.jpg) 」────
 *Hola! {},*
 *I am an Anime themed advance group management bot with a lot of Sexy Features.*
 ➖➖➖➖➖➖➖➖➖➖➖➖➖
