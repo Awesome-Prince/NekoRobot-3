7fae9e4cfbeb56040216f159bd998e91943fe7a7
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-19 13:51:40 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -739,6 +739,7 @@ def main():
 
 if __name__ == "__main__":
     LOGGER.info(f"Successfully loaded modules: {str(ALL_MODULES)}")
+    telethn.start(bot_token=TOKEN)
     pgram.start()
     main()
     idle()
