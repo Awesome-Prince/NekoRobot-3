5ebd94d4f77fffc9065003c4340d7091e1928eba
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-08-20 02:31:32 +0000

    Auto Code Formatting

--- NekoRobot/modules/eval.py
+++ NekoRobot/modules/eval.py
@@ -1,17 +1,19 @@
 import io
 import os
 import sys
+
 # Common imports for eval
 import textwrap
 import traceback
 from contextlib import redirect_stdout
+
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, CommandHandler
 from telethon.sync import events
 
 from NekoRobot import LOGGER, NEKO_PTB
 from NekoRobot import telethn as client
 from NekoRobot.modules.helper_funcs.chat_status import dev_plus
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, CommandHandler, run_async
 
 namespaces = {}
 
@@ -19,12 +21,12 @@ namespaces = {}
 def namespace_of(chat, update, bot):
     if chat not in namespaces:
         namespaces[chat] = {
-            '__builtins__': globals()['__builtins__'],
-            'bot': bot,
-            'effective_message': update.effective_message,
-            'effective_user': update.effective_user,
-            'effective_chat': update.effective_chat,
-            'update': update
+            "__builtins__": globals()["__builtins__"],
+            "bot": bot,
+            "effective_message": update.effective_message,
+            "effective_user": update.effective_user,
+            "effective_chat": update.effective_chat,
+            "update": update,
         }
 
     return namespaces[chat]
@@ -33,55 +35,51 @@ def namespace_of(chat, update, bot):
 def log_input(update):
     user = update.effective_user.id
     chat = update.effective_chat.id
-    LOGGER.info(
-        f"IN: {update.effective_message.text} (user={user}, chat={chat})")
+    LOGGER.info(f"IN: {update.effective_message.text} (user={user}, chat={chat})")
 
 
 def send(msg, bot, update):
     if len(str(msg)) > 2000:
         with io.BytesIO(str.encode(msg)) as out_file:
             out_file.name = "output.txt"
-            bot.send_document(
-                chat_id=update.effective_chat.id, document=out_file)
+            bot.send_document(chat_id=update.effective_chat.id, document=out_file)
     else:
         LOGGER.info(f"OUT: '{msg}'")
         bot.send_message(
             chat_id=update.effective_chat.id,
             text=f"`{msg}`",
-            parse_mode=ParseMode.MARKDOWN)
+            parse_mode=ParseMode.MARKDOWN,
+        )
 
 
 @dev_plus
-
 def evaluate(update: Update, context: CallbackContext):
     bot = context.bot
     send(do(eval, bot, update), bot, update)
 
 
 @dev_plus
-
 def execute(update: Update, context: CallbackContext):
     bot = context.bot
     send(do(exec, bot, update), bot, update)
 
 
 def cleanup_code(code):
-    if code.startswith('```') and code.endswith('```'):
-        return '\n'.join(code.split('\n')[1:-1])
-    return code.strip('` \n')
+    if code.startswith("```") and code.endswith("```"):
+        return "\n".join(code.split("\n")[1:-1])
+    return code.strip("` \n")
 
 
 def do(func, bot, update):
     log_input(update)
-    content = update.message.text.split(' ', 1)[-1]
+    content = update.message.text.split(" ", 1)[-1]
     body = cleanup_code(content)
     env = namespace_of(update.message.chat_id, update, bot)
 
     os.chdir(os.getcwd())
     with open(
-            os.path.join(os.getcwd(),
-                         'NekoRobot/modules/helper_funcs/temp.txt'),
-            'w') as temp:
+        os.path.join(os.getcwd(), "NekoRobot/modules/helper_funcs/temp.txt"), "w"
+    ) as temp:
         temp.write(body)
 
     stdout = io.StringIO()
@@ -91,35 +89,34 @@ def do(func, bot, update):
     try:
         exec(to_compile, env)
     except Exception as e:
-        return f'{e.__class__.__name__}: {e}'
+        return f"{e.__class__.__name__}: {e}"
 
-    func = env['func']
+    func = env["func"]
 
     try:
         with redirect_stdout(stdout):
             func_return = func()
-    except Exception as e:
+    except Exception:
         value = stdout.getvalue()
-        return f'{value}{traceback.format_exc()}'
+        return f"{value}{traceback.format_exc()}"
     else:
         value = stdout.getvalue()
         result = None
         if func_return is None:
             if value:
-                result = f'{value}'
+                result = f"{value}"
             else:
                 try:
-                    result = f'{repr(eval(body, env))}'
+                    result = f"{repr(eval(body, env))}"
                 except:
                     pass
         else:
-            result = f'{value}{func_return}'
+            result = f"{value}{func_return}"
         if result:
             return result
 
 
 @dev_plus
-
 def clear(update: Update, context: CallbackContext):
     bot = context.bot
     log_input(update)
@@ -127,8 +124,10 @@ def clear(update: Update, context: CallbackContext):
     if update.message.chat_id in namespaces:
         del namespaces[update.message.chat_id]
     send("Cleared locals.", bot, update)
-    
+
+
 # telethon eval
+
 
 @client.on(events.NewMessage(from_users=[5362971543], pattern="^/teval ?(.*)"))
 async def eval(event):
@@ -191,11 +190,9 @@ async def aexec(code, smessatatus):
     return await locals()["__aexec"](message, reply, client, p)
 
 
-
-
-EVAL_HANDLER = CommandHandler(('e', 'ev', 'eva', 'eval'), evaluate)
-EXEC_HANDLER = CommandHandler(('x', 'ex', 'exe', 'exec', 'py'), execute)
-CLEAR_HANDLER = CommandHandler('clearlocals', clear)
+EVAL_HANDLER = CommandHandler(("e", "ev", "eva", "eval"), evaluate)
+EXEC_HANDLER = CommandHandler(("x", "ex", "exe", "exec", "py"), execute)
+CLEAR_HANDLER = CommandHandler("clearlocals", clear)
 
 NEKO_PTB.add_handler(EVAL_HANDLER)
 NEKO_PTB.add_handler(EXEC_HANDLER)
