5bafaab25acbe6fba63938ec07bf91f95aef0fe4
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-23 17:23:50 +0530

    Update telegraph.py

--- NekoRobot/modules/telegraph.py
+++ NekoRobot/modules/telegraph.py
@@ -23,148 +23,95 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-from NekoRobot import telethn as tbot
 from NekoRobot.events import register
-
+from NekoRobot import telethn as tbot
 TMP_DOWNLOAD_DIRECTORY = "./"
-
-
+from telethon import events, Button, custom
 import os
-from datetime import datetime
-
 from PIL import Image
-from telegraph import Telegraph, exceptions, upload_file
-
-emilia = "EMILIA"
-
+from datetime import datetime
+from telegraph import Telegraph, upload_file, exceptions
+neko = "NEKO"
 telegraph = Telegraph()
-
-r = telegraph.create_account(short_name=emilia)
-
+r = telegraph.create_account(short_name=neko)
 auth_url = r["auth_url"]
 
 
-@register(pattern="^/tg(m|xt) ?(.*)")
+@register(pattern="^/t(m|xt) ?(.*)")
 async def _(event):
-
     if event.fwd_from:
-
         return
-
     optional_title = event.pattern_match.group(2)
-
     if event.reply_to_msg_id:
-
         start = datetime.now()
-
         r_message = await event.get_reply_message()
-
         input_str = event.pattern_match.group(1)
-
         if input_str == "m":
-
             downloaded_file_name = await tbot.download_media(
-                r_message, TMP_DOWNLOAD_DIRECTORY
+                r_message,
+                TMP_DOWNLOAD_DIRECTORY
             )
-
             end = datetime.now()
-
             ms = (end - start).seconds
-
-            h = await event.reply(
-                "Downloaded to {} in {} seconds.".format(downloaded_file_name, ms)
-            )
-
+            h = await event.reply("Downloaded {} in {} seconds.".format(downloaded_file_name, ms))
             if downloaded_file_name.endswith((".webp")):
-
                 resize_image(downloaded_file_name)
-
             try:
-
                 start = datetime.now()
-
                 media_urls = upload_file(downloaded_file_name)
-
             except exceptions.TelegraphException as exc:
-
                 await h.edit("ERROR: " + str(exc))
-
                 os.remove(downloaded_file_name)
-
             else:
-
                 end = datetime.now()
-
-                (end - start).seconds
-
+                BUTTON = [[Button.url("Telegraph", f"https://telegra.ph/{media_urls[0]}")]]
+                ms_two = (end - start).seconds
                 os.remove(downloaded_file_name)
-
-                await h.edit(
-                    "Uploaded to https://te.legra.ph{}".format(media_urls[0]),
-                    link_preview=True,
-                )
-
+                await h.delete()
+                await event.reply("Preview For [{}](tg://user?id={})[.](https://telegra.ph{})".format(event.sender.first_name, event.sender.id, media_urls[0]), link_preview=True, buttons=BUTTON)
         elif input_str == "xt":
-
             user_object = await tbot.get_entity(r_message.sender_id)
-
-            title_of_page = user_object.first_name  # + " " + user_object.last_name
-
+            title_of_page = user_object.first_name # + " " + user_object.last_name
             # apparently, all Users do not have last_name field
-
             if optional_title:
-
                 title_of_page = optional_title
-
             page_content = r_message.message
-
             if r_message.media:
-
                 if page_content != "":
-
                     title_of_page = page_content
-
                 downloaded_file_name = await tbot.download_media(
-                    r_message, TMP_DOWNLOAD_DIRECTORY
+                    r_message,
+                    TMP_DOWNLOAD_DIRECTORY
                 )
-
                 m_list = None
-
                 with open(downloaded_file_name, "rb") as fd:
-
                     m_list = fd.readlines()
-
                 for m in m_list:
-
                     page_content += m.decode("UTF-8") + "\n"
-
                 os.remove(downloaded_file_name)
-
             page_content = page_content.replace("\n", "<br>")
-
-            response = telegraph.create_page(title_of_page, html_content=page_content)
-
-            end = datetime.now()
-
-            ms = (end - start).seconds
-
-            await event.reply(
-                "Pasted to https://telegra.ph/{} in {} seconds.".format(
-                    response["path"], ms
-                ),
-                link_preview=True,
+            response = telegraph.create_page(
+                title_of_page,
+                html_content=page_content
             )
-
+            end = datetime.now()
+            ms = (end - start).seconds
+            # h = await event.reply("Downloaded in {} seconds.".format(ms))
+            # await h.delete()
+            # BUUTON = [[Button.url("Telegraph", f"https://telegra.ph/{response["path"]}")]]
+            await event.reply("Pasted to [Telegraph](https://telegra.ph/{}) in {} seconds.".format(response["path"], ms), link_preview=True)
     else:
-
         await event.reply("Reply to a message to get a permanent telegra.ph link.")
 
 
 def resize_image(image):
-
     im = Image.open(image)
-
     im.save(image, "PNG")
 
 
-__mod_name__ = "telegraph"
+__help__ = """
+ ‣ /tm :Get Telegraph Link Of Replied Media
+ ‣ /txt :Get Telegraph Link of Replied Text
+"""
+
+__mod_name__ = "Telegraph"
