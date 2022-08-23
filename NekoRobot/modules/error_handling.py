63cf861836c160bb3ef5c924562628e7aa0b78a7
SOME1HING <yashprakash2005@gmail.com>
2022-08-23 19:51:10 +0530

    Update error_handling.py

--- NekoRobot/modules/error_handling.py
+++ NekoRobot/modules/error_handling.py
@@ -1,3 +1,30 @@
+"""
+STATUS: Code is working. ✅
+"""
+
+"""
+GNU General Public License v3.0
+
+Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
+
+Credits:-
+    I don't know who originally wrote this code. If you originally wrote this code, please reach out to me. 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import asyncio
 import html
 import io
 import random
@@ -5,11 +32,11 @@ import sys
 import traceback
 
 import pretty_errors
-import requests
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext, CommandHandler
 
-from NekoRobot import DEV_USERS, ERROR_LOGS, NEKO_PTB
+from NekoRobot import dispatcher, DEV_USERS, ERROR_LOGS
+from ..utils.pastebin import paste
 
 pretty_errors.mono()
 
@@ -34,78 +61,60 @@ class ErrorsDict(dict):
     def __len__(self):
         return len(self.raw)
 
-
 errors = ErrorsDict()
-
 
 def error_callback(update: Update, context: CallbackContext):
     if not update:
         return
-    if context.error in errors:
-        return
-    try:
-        stringio = io.StringIO()
-        pretty_errors.output_stderr = stringio
-        output = pretty_errors.excepthook(
-            type(context.error),
+    if context.error not in errors:
+        try:
+            stringio = io.StringIO()
+            pretty_errors.output_stderr = stringio
+            output = pretty_errors.excepthook(
+                type(context.error),
+                context.error,
+                context.error.__traceback__,
+            )
+            pretty_errors.output_stderr = sys.stderr
+            pretty_error = stringio.getvalue()
+            stringio.close()
+        except:
+            pretty_error = "Failed to create pretty error."
+        tb_list = traceback.format_exception(
+            None,
             context.error,
             context.error.__traceback__,
         )
-        pretty_errors.output_stderr = sys.stderr
-        pretty_error = stringio.getvalue()
-        stringio.close()
-    except:
-        pretty_error = "Failed to create pretty error."
-    tb_list = traceback.format_exception(
-        None,
-        context.error,
-        context.error.__traceback__,
-    )
-    tb = "".join(tb_list)
-    pretty_message = (
-        "{}\n"
-        "-------------------------------------------------------------------------------\n"
-        "An exception was raised while handling an update\n"
-        "User: {}\n"
-        "Chat: {} {}\n"
-        "Callback data: {}\n"
-        "Message: {}\n\n"
-        "Full Traceback: {}"
-    ).format(
-        pretty_error,
-        update.effective_user.id,
-        update.effective_chat.title if update.effective_chat else "",
-        update.effective_chat.id if update.effective_chat else "",
-        update.callback_query.data if update.callback_query else "None",
-        update.effective_message.text if update.effective_message else "No message",
-        tb,
-    )
-    key = requests.post(
-        "https://www.toptal.com/developers/hastebin/documents",
-        data=pretty_message.encode("UTF-8"),
-    ).json()
-    e = html.escape(f"{context.error}")
-    if not key.get("key"):
-        with open("error.txt", "w+") as f:
-            f.write(pretty_message)
-        context.bot.send_document(
+        tb = "".join(tb_list)
+        pretty_message = (
+            "{}\n"
+            "-------------------------------------------------------------------------------\n"
+            "An exception was raised while handling an update\n"
+            "User: {}\n"
+            "Chat: {} {}\n"
+            "Callback data: {}\n"
+            "Message: {}\n\n"
+            "Full Traceback: {}"
+        ).format(
+            pretty_error,
+            update.effective_user.id,
+            update.effective_chat.title if update.effective_chat else "",
+            update.effective_chat.id if update.effective_chat else "",
+            update.callback_query.data if update.callback_query else "None",
+            update.effective_message.text if update.effective_message else "No message",
+            tb,
+        )
+        e = html.escape(f"{context.error}")
+        link = asyncio.run(paste(pretty_message))
+        context.bot.send_message(
             ERROR_LOGS,
-            open("error.txt", "rb"),
-            caption=f"#{context.error.identifier}\n<b>An unknown error occured:</b>\n<code>{e}</code>",
+            text=f"#{context.error.identifier}\n<b>An Error has occurred:"
+            f"</b>\n<code>{e}</code>",
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("See Errors", url=link)]],
+            ),
             parse_mode="html",
         )
-        return
-    key = key.get("key")
-    url = f"https://www.toptal.com/developers/hastebin/{key}"
-    context.bot.send_message(
-        ERROR_LOGS,
-        text=f"#{context.error.identifier}\n<b>An unknown error occured:</b>\n<code>{e}</code>",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("HasteBin", url=url)]],
-        ),
-        parse_mode="html",
-    )
-
 
 def list_errors(update: Update, context: CallbackContext):
     if update.effective_user.id not in DEV_USERS:
@@ -127,6 +136,5 @@ def list_errors(update: Update, context: CallbackContext):
         return
     update.effective_message.reply_text(msg, parse_mode="html")
 
-
-NEKO_PTB.add_error_handler(error_callback)
-NEKO_PTB.add_handler(CommandHandler("errors", list_errors))
+dispatcher.add_error_handler(error_callback)
+dispatcher.add_handler(CommandHandler("errors", list_errors))
