014c995926180a0aa4a316067546fc55f1a2d891
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-25 13:32:51 +0530

    Update alive.py

--- NekoRobot/modules/alive.py
+++ NekoRobot/modules/alive.py
@@ -74,7 +74,7 @@ def awake(update: Update, context: CallbackContext):
     current_time = datetime.utcnow()
     uptime_sec = (current_time - START_TIME).total_seconds()
     uptime = _human_time_duration(int(uptime_sec))
-    NekoX = f"* ♡ Hey [{user.first_name}](tg://user?id={user.id}),*\n\n"
+    NekoX = f"* ♡ Hey [{user.first_name}](tg://user?id={user.id})*"
     NekoX += f"*I'm {BOT_NAME}*\n\n"
     NekoX += "➖➖➖➖➖➖➖➖➖➖➖➖➖\n\n"
     NekoX += f"*♡ My Uptime :* `{uptime}`\n\n"
