250dd3d2be61a380f9b753b30611bfa73218f921
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-25 13:28:19 +0530

    Update alive.py

--- NekoRobot/modules/alive.py
+++ NekoRobot/modules/alive.py
@@ -1,24 +1,26 @@
 """
-STATUS: Code is working. ✅
-"""
-
-"""
- BSD-2-Clause license
-
-Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+BSD 2-Clause License
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince ]
+Copyright (c) 2022-2023, BlackLover • Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import asyncio
@@ -74,11 +76,11 @@ def awake(update: Update, context: CallbackContext):
     uptime = _human_time_duration(int(uptime_sec))
     NekoX = f"* ♡ Hey [{user.first_name}](tg://user?id={user.id}),*\n\n"
     NekoX += f"*I'm {BOT_NAME}*\n\n"
-    NekoX += "➖➖➖➖➖➖➖➖➖➖➖➖➖"
+    NekoX += "➖➖➖➖➖➖➖➖➖➖➖➖➖\n\n"
     NekoX += f"*♡ My Uptime :* `{uptime}`\n\n"
     NekoX += f"*♡ Python Version :* `{python_version()}`\n\n"
     NekoX += "*♡ My Master :* [LovelyPrince](https://t.me/BlackLover_Prince)\n\n"
-    NekoX += "➖➖➖➖➖➖➖➖➖➖➖➖➖"
+    NekoX += "➖➖➖➖➖➖➖➖➖➖➖➖➖\n\n"
     NekoX += f"*Thanks For Adding Me In* {chat_name}"
     buttons = [
         [
