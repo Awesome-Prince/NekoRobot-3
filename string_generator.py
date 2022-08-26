64c48d0d10a32a1d175c0e8a4dc912beccb5482c
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-26 22:11:21 +0530

    `Convert pyro to tele`

--- string_generator.py
+++ string_generator.py
@@ -1,5 +1,5 @@
-from pyrogram.sessions import StringSession
-from pyrogram.sync import TelegramClient
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
 
 print(
     """Please go-to my.telegram.org
