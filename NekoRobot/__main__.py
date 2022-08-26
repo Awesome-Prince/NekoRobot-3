65ab39a5971d046ac3de19b20079fd26884cfec7
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-26 18:31:27 +0530

    `re-starting telethon client`

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -762,7 +762,6 @@ def main():
 
 if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
-    tbot.start(bot_token=TOKEN)
     pgram.start()
     main()
     idle()
