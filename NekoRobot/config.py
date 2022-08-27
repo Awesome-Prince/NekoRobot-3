74d2ebaf0cdc67181a2e5f447d13df4d8a547b0b
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-27 21:39:42 +0530

    Update config.py

--- NekoRobot/config.py
+++ NekoRobot/config.py
@@ -96,7 +96,7 @@ class Config(object):
 
     API_ID = 1234567  # integer value, dont use ""
     API_HASH = "12345abcdefg23459"
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = "5794945691:AAEkJf3WxkPEQp39dRXkkCedVfmtHJOiF3s"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1732814103  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "DarlingPrince"
     SUPPORT_CHAT = "BlackLover_Support"  # Your own group for support, do not add the @
