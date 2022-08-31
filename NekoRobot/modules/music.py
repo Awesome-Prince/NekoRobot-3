3d17dbab9fcd9d04510fd6220f090a50163960ed
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-31 13:06:12 +0530

    Update music.py

--- NekoRobot/modules/music.py
+++ NekoRobot/modules/music.py
@@ -200,12 +200,6 @@ __help__ = """
 ➛ /dplay <song name> - play song you requested via deezer
 ➛ /splay <song name> - play song you requested via jio saavn
 ➛ /playlist - Show now playing list
-➛ /current - Show now playing
-➛ /song <song name> - download songs you want quickly
-➛ /search <query> - search videos on youtube with details
-➛ /deezer <song name> - download songs you want quickly via deezer
-➛ /saavn <song name> - download songs you want quickly via saavn
-➛ /video <song name> - download videos you want quickly
 
 ❍ *Admins Command*
 
@@ -215,7 +209,6 @@ __help__ = """
 ➛ /skip - play next song
 ➛ /end - stop music play
 ➛ /userbotjoin - invite assistant to your chat
-➛ /admincache - Refresh admin list
 
 ❍ *Guidelines*
 
