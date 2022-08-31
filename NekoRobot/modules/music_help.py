f6597c628022903000ebd6ec8a5ee728f06265c0
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-08-31 11:38:06 +0530

    `added help string for music`

---
+++ NekoRobot/modules/music_help.py
@@ -0,0 +1,32 @@
+__mod_name__ = "Music"
+
+__help__ = """
+
+❍ *Play Music* ✨🎧
+
+➛ /play <song name> - play song you requested
+➛ /dplay <song name> - play song you requested via deezer
+➛ /splay <song name> - play song you requested via jio saavn
+➛ /playlist - Show now playing list
+➛ /current - Show now playing
+➛ /song <song name> - download songs you want quickly
+➛ /search <query> - search videos on youtube with details
+➛ /deezer <song name> - download songs you want quickly via deezer
+➛ /saavn <song name> - download songs you want quickly via saavn
+➛ /video <song name> - download videos you want quickly
+
+❍ *Admins Command*
+
+➛ /player - open music player settings panel
+➛ /pause - pause song play
+➛ /resume - resume song play
+➛ /skip - play next song
+➛ /end - stop music play
+➛ /userbotjoin - invite assistant to your chat
+➛ /admincache - Refresh admin list
+
+❍ *Guidelines*
+
+1) Add @Awesome_Neko to your group to admin with all permission
+2) Use /play <song name> and enjoy the music✨🥀
+"""
