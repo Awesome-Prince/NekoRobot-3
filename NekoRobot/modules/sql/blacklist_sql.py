63148d2661d63b89708a890ff4f32657f1fb6fc0
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-09-04 04:06:03 +0000

    Auto Code Formatting

--- NekoRobot/modules/sql/blacklist_sql.py
+++ NekoRobot/modules/sql/blacklist_sql.py
@@ -25,9 +25,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import threading
 
-from sqlalchemy import func, distinct, Column, String, UnicodeText, BigInteger
+from sqlalchemy import BigInteger, Column, String, UnicodeText, distinct, func
 
-from NekoRobot.modules.sql import SESSION, BASE
+from NekoRobot.modules.sql import BASE, SESSION
 
 
 class BlackListFilters(BASE):
@@ -44,8 +44,10 @@ class BlackListFilters(BASE):
 
     def __eq__(self, other):
         return bool(
-            isinstance(other, BlackListFilters) and
-            self.chat_id == other.chat_id and self.trigger == other.trigger)
+            isinstance(other, BlackListFilters)
+            and self.chat_id == other.chat_id
+            and self.trigger == other.trigger
+        )
 
 
 class BlacklistSettings(BASE):
@@ -61,7 +63,8 @@ class BlacklistSettings(BASE):
 
     def __repr__(self):
         return "<{} will executing {} for blacklist trigger.>".format(
-            self.chat_id, self.blacklist_type)
+            self.chat_id, self.blacklist_type
+        )
 
 
 BlackListFilters.__table__.create(checkfirst=True)
@@ -89,11 +92,9 @@ def add_to_blacklist(chat_id, trigger):
 
 def rm_from_blacklist(chat_id, trigger):
     with BLACKLIST_FILTER_INSERTION_LOCK:
-        blacklist_filt = SESSION.query(BlackListFilters).get(
-            (str(chat_id), trigger))
+        blacklist_filt = SESSION.query(BlackListFilters).get((str(chat_id), trigger))
         if blacklist_filt:
-            if trigger in CHAT_BLACKLISTS.get(str(chat_id),
-                                              set()):  # sanity check
+            if trigger in CHAT_BLACKLISTS.get(str(chat_id), set()):  # sanity check
                 CHAT_BLACKLISTS.get(str(chat_id), set()).remove(trigger)
 
             SESSION.delete(blacklist_filt)
@@ -117,16 +118,18 @@ def num_blacklist_filters():
 
 def num_blacklist_chat_filters(chat_id):
     try:
-        return (SESSION.query(BlackListFilters.chat_id).filter(
-            BlackListFilters.chat_id == str(chat_id)).count())
+        return (
+            SESSION.query(BlackListFilters.chat_id)
+            .filter(BlackListFilters.chat_id == str(chat_id))
+            .count()
+        )
     finally:
         SESSION.close()
 
 
 def num_blacklist_filter_chats():
     try:
-        return SESSION.query(func.count(distinct(
-            BlackListFilters.chat_id))).scalar()
+        return SESSION.query(func.count(distinct(BlackListFilters.chat_id))).scalar()
     finally:
         SESSION.close()
 
@@ -146,7 +149,8 @@ def set_blacklist_strength(chat_id, blacklist_type, value):
         curr_setting = SESSION.query(BlacklistSettings).get(str(chat_id))
         if not curr_setting:
             curr_setting = BlacklistSettings(
-                chat_id, blacklist_type=int(blacklist_type), value=value)
+                chat_id, blacklist_type=int(blacklist_type), value=value
+            )
 
         curr_setting.blacklist_type = int(blacklist_type)
         curr_setting.value = str(value)
@@ -205,8 +209,10 @@ def __load_chat_settings_blacklists():
 def migrate_chat(old_chat_id, new_chat_id):
     with BLACKLIST_FILTER_INSERTION_LOCK:
         chat_filters = (
-            SESSION.query(BlackListFilters).filter(
-                BlackListFilters.chat_id == str(old_chat_id)).all())
+            SESSION.query(BlackListFilters)
+            .filter(BlackListFilters.chat_id == str(old_chat_id))
+            .all()
+        )
         for filt in chat_filters:
             filt.chat_id = str(new_chat_id)
         SESSION.commit()
