8e3db8b7dae1e921d38ea1c4ad462888246e0f0f
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-04 21:51:18 +0530

    Update alive.py

--- NekoRobot/modules/alive.py
+++ NekoRobot/modules/alive.py
@@ -44,32 +44,11 @@ PRINCE = (
 )
 
 
-START_TIME = datetime.utcnow()
-START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
-TIME_DURATION_UNITS = (
-    ("week", 60 * 60 * 24 * 7),
-    ("day", 60 * 60 * 24),
-    ("hour", 60 * 60),
-    ("min", 60),
-    ("sec", 1),
-)
-
-
-def _human_time_duration(seconds):
-    if seconds == 0:
-        return "inf"
-    parts = []
-    for unit, div in TIME_DURATION_UNITS:
-        amount, seconds = divmod(int(seconds), div)
-        if amount > 0:
-            parts.append(f'{amount} {unit}{"" if amount == 1 else "s"}')
-    return ", ".join(parts)
 
 
 @register(pattern=("/alive"))
 async def alive(event):
     NEKO = f"** ♡ Hey [{event.sender.first_name}](tg://user?id={event.sender.id}) I,m NekoRobot **\n\n"
-    NEKO += f"**♡ My Uptime :** `{uptime}`\n\n"
     NEKO += f"**♡ Python Version :** `{python_version}`\n\n"
     NEKO += f"**♡ Telethon Version :** `{tlhver}`\n\n"
     NEKO += f"**♡ Pyrogram Version :** `{pyrover}`\n\n"
