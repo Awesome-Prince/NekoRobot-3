786f0ae9a12c8cb77587b3c8832d2dffd0b855c4
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-09-04 04:07:22 +0000

    Auto Code Formatting

--- NekoRobot/modules/sql/blacklistusers_sql.py
+++ NekoRobot/modules/sql/blacklistusers_sql.py
@@ -25,8 +25,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import threading
 
-from NekoRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Column, String, UnicodeText
+
+from NekoRobot.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
@@ -85,9 +86,7 @@ def is_user_blacklisted(user_id):
 def __load_blacklist_userid_list():
     global BLACKLIST_USERS
     try:
-        BLACKLIST_USERS = {
-            int(x.user_id) for x in SESSION.query(BlacklistUsers).all()
-        }
+        BLACKLIST_USERS = {int(x.user_id) for x in SESSION.query(BlacklistUsers).all()}
     finally:
         SESSION.close()
 
