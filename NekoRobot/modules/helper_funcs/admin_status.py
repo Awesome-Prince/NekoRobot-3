dadcd064d4a3bac8d101f7ba7d979e34ab49dede
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-09-07 14:56:47 +0000

    Auto Code Formatting

--- NekoRobot/modules/helper_funcs/admin_status.py
+++ NekoRobot/modules/helper_funcs/admin_status.py
@@ -1,25 +1,29 @@
 from functools import wraps
-from typing import Optional
 from threading import RLock
 
-from telegram import Chat, Update, ChatMember, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import (
+    Chat,
+    ChatMember,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Update,
+)
 from telegram.constants import ParseMode
-from telegram.ext import CallbackContext as Ctx, CallbackQueryHandler as CBHandler
+from telegram.ext import CallbackContext as Ctx
+from telegram.ext import CallbackQueryHandler as CBHandler
 
 from NekoRobot import NEKO_PTB
 
-from .admin_status_helpers import (
-	ADMINS_CACHE as A_CACHE,
-	BOT_ADMIN_CACHE as B_CACHE,
-	DRAGONS,
-	AdminPerms,
-	anon_callbacks as a_cb,
-	edit_anon_msg as eam,
-    button_expired_error as bxp,
-)
+from .admin_status_helpers import ADMINS_CACHE as A_CACHE
+from .admin_status_helpers import BOT_ADMIN_CACHE as B_CACHE
+from .admin_status_helpers import AdminPerms
+from .admin_status_helpers import anon_callbacks as a_cb
+from .admin_status_helpers import button_expired_error as bxp
+from .admin_status_helpers import edit_anon_msg as eam
 
 anon_callbacks = {}
 anon_callback_messages = {}
+
 
 def bot_is_admin(chat: Chat, bot_id: int, bot_member: ChatMember = None) -> bool:
     if chat.type == "private" or chat.all_members_are_administrators:
@@ -35,36 +39,44 @@ def bot_is_admin(chat: Chat, bot_id: int, bot_member: ChatMember = None) -> bool
 # @bot_perm_check() with no perm to check for admin-ship only
 # or as @bot_perm_check(AdminPerms.value) to check for a specific permission
 def bot_admin_check(permission: AdminPerms = None):
-	def wrapper(func):
-		@wraps(func)
-		async def wrapped(update: Update, context: Ctx, *args, **kwargs):
-			nonlocal permission
-			chat = update.effective_chat
-			if chat.type == "private" or chat.all_members_are_administrators:
-				return func(update, context, *args, **kwargs)
-			bot_id = {BOT_ID}
+    def wrapper(func):
+        @wraps(func)
+        async def wrapped(update: Update, context: Ctx, *args, **kwargs):
+            nonlocal permission
+            chat = update.effective_chat
+            if chat.type == "private" or chat.all_members_are_administrators:
+                return func(update, context, *args, **kwargs)
+            bot_id = {BOT_ID}
 
-			try:  # try to get from cache
-				bot_member = B_CACHE[chat.id]
-			except (KeyError, IndexError):  # if not in cache, get from API and save to cache
-				bot_member = NEKO_PTB.bot.getChatMember(chat.id, bot_id)
-				B_CACHE[chat.id] = bot_member
+            try:  # try to get from cache
+                bot_member = B_CACHE[chat.id]
+            except (
+                KeyError,
+                IndexError,
+            ):  # if not in cache, get from API and save to cache
+                bot_member = NEKO_PTB.bot.getChatMember(chat.id, bot_id)
+                B_CACHE[chat.id] = bot_member
 
-			if permission:  # if a perm is required, check for it
-				if getattr(bot_member, permission.value):
-					func(update, context, *args, **kwargs)
-					return
-				return await update.effective_message.reply_text(
-						f"I can't perform this action due to missing permissions;\n"
-						f"Make sure i am an admin and {permission.name.lower().replace('is_', 'am ').replace('_', ' ')}!")
+            if permission:  # if a perm is required, check for it
+                if getattr(bot_member, permission.value):
+                    func(update, context, *args, **kwargs)
+                    return
+                return await update.effective_message.reply_text(
+                    f"I can't perform this action due to missing permissions;\n"
+                    f"Make sure i am an admin and {permission.name.lower().replace('is_', 'am ').replace('_', ' ')}!"
+                )
 
-			if bot_member.status == "administrator":  # if no perm is required, check for admin-ship only
-				return func(update, context, *args, **kwargs)
-			return await update.effective_message.reply_text("I can't perform this action because I'm not admin!")
+            if (
+                bot_member.status == "administrator"
+            ):  # if no perm is required, check for admin-ship only
+                return func(update, context, *args, **kwargs)
+            return await update.effective_message.reply_text(
+                "I can't perform this action because I'm not admin!"
+            )
 
-		return wrapped
+        return wrapped
 
-	return wrapper
+    return wrapper
 
 
 def user_is_admin(
@@ -72,44 +84,52 @@ def user_is_admin(
     user_id: int,
     channels: bool = False,  # if True, returns True if user is anonymous
     allow_moderators: bool = False,  # if True, returns True if user is a moderator
-    perm: AdminPerms = None  # if not None, returns True if user has the specified permission
-    ) -> bool:
-	chat = update.effective_chat
-	if chat.type == "private" or user_id in (SUDO_USERS if allow_moderators else SUDO_USERS):
-		return True
+    perm: AdminPerms = None,  # if not None, returns True if user has the specified permission
+) -> bool:
+    chat = update.effective_chat
+    if chat.type == "private" or user_id in (
+        SUDO_USERS if allow_moderators else SUDO_USERS
+    ):
+        return True
 
-	if channels and (update.effective_message.sender_chat is not None and update.effective_message.sender_chat.type != "channel"):
-		return True  # return true if user is anonymous
+    if channels and (
+        update.effective_message.sender_chat is not None
+        and update.effective_message.sender_chat.type != "channel"
+    ):
+        return True  # return true if user is anonymous
 
-	member: ChatMember = get_mem_from_cache(user_id, chat.id)
+    member: ChatMember = get_mem_from_cache(user_id, chat.id)
 
-	if not member:  # not in cache so not an admin
-		return False
+    if not member:  # not in cache so not an admin
+        return False
 
-	if perm:  # check perm if its required
-		try:
-			the_perm = perm.value
-		except AttributeError:
-			return bxp(update)
-		return getattr(member, the_perm) or member.status == "creator"
+    if perm:  # check perm if its required
+        try:
+            the_perm = perm.value
+        except AttributeError:
+            return bxp(update)
+        return getattr(member, the_perm) or member.status == "creator"
 
-	return member.status in ["administrator", "creator"]  # check if user is admin
+    return member.status in ["administrator", "creator"]  # check if user is admin
 
 
 RLOCK = RLock()
 
 
 async def get_mem_from_cache(user_id: int, chat_id: int) -> ChatMember:
-	with RLOCK:
-		try:
-			for i in A_CACHE[chat_id]:
-				if i.user.id == user_id: return i
+    with RLOCK:
+        try:
+            for i in A_CACHE[chat_id]:
+                if i.user.id == user_id:
+                    return i
 
-		except (KeyError, IndexError):
-			admins = await NEKO_PTB.bot.getChatAdministrators(chat_id)
-			A_CACHE[chat_id] = admins
-			for i in admins:
-				if i.user.id == user_id: return i
+        except (KeyError, IndexError):
+            admins = await NEKO_PTB.bot.getChatAdministrators(chat_id)
+            A_CACHE[chat_id] = admins
+            for i in admins:
+                if i.user.id == user_id:
+                    return i
+
 
 def user_admin_check(permission: AdminPerms = None):
     def wrapper(func):
@@ -166,45 +186,48 @@ def user_admin_check(permission: AdminPerms = None):
 
 # decorator, can be used as @user_not_admin_check to check user is not admin
 def user_not_admin_check(func):
-	@wraps(func)
-	def wrapped(update: Update, context: Ctx, *args, **kwargs):
-		message = update.effective_message
-		user = message.sender_chat or update.effective_user
-		if (message.is_automatic_forward
-				or (message.sender_chat and message.sender_chat.type != "channel")
-				or not user):
-			return
-		if not user_is_admin(update, user.id, channels = True):
-			return func(update, context, *args, **kwargs)
-		return
-	return wrapped
+    @wraps(func)
+    def wrapped(update: Update, context: Ctx, *args, **kwargs):
+        message = update.effective_message
+        user = message.sender_chat or update.effective_user
+        if (
+            message.is_automatic_forward
+            or (message.sender_chat and message.sender_chat.type != "channel")
+            or not user
+        ):
+            return
+        if not user_is_admin(update, user.id, channels=True):
+            return func(update, context, *args, **kwargs)
+        return
+
+    return wrapped
 
 
 def perm_callback_check(upd: Update, _: Ctx):
     callback = upd.callback_query
-    chat_id = int(callback.data.split('/')[1])
-    message_id = int(callback.data.split('/')[2])
-    perm = callback.data.split('/')[3]
+    chat_id = int(callback.data.split("/")[1])
+    message_id = int(callback.data.split("/")[2])
+    perm = callback.data.split("/")[3]
     user_id = callback.from_user.id
     msg = upd.effective_message
 
-    mem = user_is_admin(upd, user_id, perm = perm if perm != 'None' else None)
+    mem = user_is_admin(upd, user_id, perm=perm if perm != "None" else None)
 
     if not mem:  # not admin or doesn't have the required perm
         eam(
             msg,
             "You need to be an admin to perform this action!"
-            if perm != 'None'
+            if perm != "None"
             else f"You lack the permission: `{perm}`!",
         )
 
         return
 
     try:
-    	cb = a_cb.pop((chat_id, message_id), None)
+        cb = a_cb.pop((chat_id, message_id), None)
     except (KeyError, IndexError):
-    	eam(msg, "This message is no longer valid.")
-    	return
+        eam(msg, "This message is no longer valid.")
+        return
 
     msg.delete()
 
@@ -214,4 +237,5 @@ def perm_callback_check(upd: Update, _: Ctx):
 
     return cb[1](cb[0][0], cb[0][1])  # return func(update, context)
 
-NEKO_PTB.add_handler(CBHandler(perm_callback_check, pattern = "anonCB"))
+
+NEKO_PTB.add_handler(CBHandler(perm_callback_check, pattern="anonCB"))
