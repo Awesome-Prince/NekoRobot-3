bccf8a6e8bf300ace14f4279bc1605cf76afe358
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-09-07 15:07:01 +0000

    Auto Code Formatting

--- NekoRobot/modules/helper_funcs/admin_status_helpers.py
+++ NekoRobot/modules/helper_funcs/admin_status_helpers.py
@@ -1,17 +1,23 @@
 from enum import Enum
-from cachetools import TTLCache
 from time import perf_counter
 
-from telegram import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, Update, Message
-from telegram import ParseMode
+from cachetools import TTLCache
+from telegram import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+    ParseMode,
+    Update,
+)
 
-from NekoRobot import DEV_USERS, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS
+from NekoRobot import DEV_USERS, SUDO_USERS
 
 # stores admin in memory for 10 min.
-ADMINS_CACHE = TTLCache(maxsize = 512, ttl = (60 * 30), timer=perf_counter)
+ADMINS_CACHE = TTLCache(maxsize=512, ttl=(60 * 30), timer=perf_counter)
 
 # stores bot admin status in memory for 10 min.
-BOT_ADMIN_CACHE = TTLCache(maxsize = 512, ttl = (60 * 10), timer=perf_counter)
+BOT_ADMIN_CACHE = TTLCache(maxsize=512, ttl=(60 * 10), timer=perf_counter)
 
 SUDO_USERS = SUDO_USERS + DEV_USERS
 
@@ -21,18 +27,18 @@ DEMONS = DEMONS + SUDO_USERS
 
 
 class AdminPerms(Enum):
-	CAN_RESTRICT_MEMBERS = 'Can Restrict Members'
-	CAN_PROMOTE_MEMBERS = 'Can Promote Members'
-	CAN_INVITE_USERS = 'Can Invite Users'
-	CAN_DELETE_MESSAGES = 'Can Delete Messages'
-	CAN_CHANGE_INFO = 'Can Change Info'
-	CAN_PIN_MESSAGES = 'Can Pin Messages'
-	IS_ANONYMOUS = 'Is Anonymous'
+    CAN_RESTRICT_MEMBERS = "Can Restrict Members"
+    CAN_PROMOTE_MEMBERS = "Can Promote Members"
+    CAN_INVITE_USERS = "Can Invite Users"
+    CAN_DELETE_MESSAGES = "Can Delete Messages"
+    CAN_CHANGE_INFO = "Can Change Info"
+    CAN_PIN_MESSAGES = "Can Pin Messages"
+    IS_ANONYMOUS = "Is Anonymous"
 
 
 class ChatStatus(Enum):
-	CREATOR = "Creator"
-	ADMIN = "Administrator"
+    CREATOR = "Creator"
+    ADMIN = "Administrator"
 
 
 # class SuperUsers(Enum):
@@ -46,40 +52,39 @@ class ChatStatus(Enum):
 
 
 def anon_reply_markup(cb_id: str) -> InlineKeyboardMarkup:
-	return InlineKeyboardMarkup(
-			[
-				[
-					InlineKeyboardButton(
-							text = 'Prove identity',
-							callback_data = cb_id
-					)
-				]
-			]
-	)
+    return InlineKeyboardMarkup(
+        [[InlineKeyboardButton(text="Prove identity", callback_data=cb_id)]]
+    )
 
 
-anon_reply_text = "Seems like you're anonymous, click the button below to prove your identity"
+anon_reply_text = (
+    "Seems like you're anonymous, click the button below to prove your identity"
+)
 
 
 def edit_anon_msg(msg: Message, text: str):
-	"""
-	edit anon check message and remove the button
-	"""
-	msg.edit_text(text, parse_mode = ParseMode.MARKDOWN, reply_markup = None)
+    """
+    edit anon check message and remove the button
+    """
+    msg.edit_text(text, parse_mode=ParseMode.MARKDOWN, reply_markup=None)
 
 
-async def user_is_not_admin_errmsg(msg: Message, permission: AdminPerms = None, cb: CallbackQuery = None):
-	errmsg = f"You are missing the following rights to use this command:\n*{permission.value}*"
-	if cb:
-		return cb.answer(errmsg, show_alert = True)
-	return await msg.reply_text(errmsg, parse_mode = ParseMode.MARKDOWN)
+async def user_is_not_admin_errmsg(
+    msg: Message, permission: AdminPerms = None, cb: CallbackQuery = None
+):
+    errmsg = f"You are missing the following rights to use this command:\n*{permission.value}*"
+    if cb:
+        return cb.answer(errmsg, show_alert=True)
+    return await msg.reply_text(errmsg, parse_mode=ParseMode.MARKDOWN)
+
 
 def button_expired_error(u: Update):
-	errmsg = "This button has expired!"
-	if u.callback_query:
-		u.callback_query.answer(errmsg, show_alert = True)
-		u.effective_message.delete()
-		return
-	return u.effective_message.edit_text(errmsg, parse_mode = ParseMode.MARKDOWN)
+    errmsg = "This button has expired!"
+    if u.callback_query:
+        u.callback_query.answer(errmsg, show_alert=True)
+        u.effective_message.delete()
+        return
+    return u.effective_message.edit_text(errmsg, parse_mode=ParseMode.MARKDOWN)
+
 
 anon_callbacks = {}
