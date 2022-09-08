8f6fffe1a84fbe634be8d0d63463a5162fa38164
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-08 12:00:08 +0530

    Update alternate.py

--- NekoRobot/modules/helper_funcs/alternate.py
+++ NekoRobot/modules/helper_funcs/alternate.py
@@ -1,37 +1,52 @@
+"""
+BSD 2-Clause License
+
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince ]
+Copyright (c) 2022-2023, Programmer • Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 from functools import wraps
-
-from telegram import ChatAction
-from telegram.error import BadRequest
+from telegram import error
 
 
-async def send_message(message, text, *args, **kwargs):
+async def send_message(message, *args, **kwargs):
     try:
-        return message.reply_text(text, *args, **kwargs)
-    except BadRequest as err:
+        return await message.reply_text(*args, **kwargs)
+    except error.BadRequest as err:
         if str(err) == "Reply message not found":
-            return message.reply_text(text, quote=False, *args, **kwargs)
+            return await message.reply_text(quote=False, *args, **kwargs)
 
 
-async def typing_action(func):
-    """Sends typing action while processing func command."""
-
-    @wraps(func)
-    async def command_func(update, context, *args, **kwargs):
-        context.bot.send_chat_action(
-            chat_id=update.effective_chat.id, action=ChatAction.TYPING
-        )
-        return func(update, context, *args, **kwargs)
-
-    return command_func
-
-
-async def send_action(action):
+def send_action(action):
     """Sends `action` while processing func command."""
 
-    async def decorator(func):
+    def decorator(func):
         @wraps(func)
         async def command_func(update, context, *args, **kwargs):
-            context.bot.send_chat_action(
+            await context.bot.send_chat_action(
                 chat_id=update.effective_chat.id, action=action
             )
             return func(update, context, *args, **kwargs)
