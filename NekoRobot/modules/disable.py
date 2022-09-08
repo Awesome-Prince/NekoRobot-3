01637167b5c5603e1be5739942267a374f1b8bc7
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-09-08 07:02:05 +0000

    Push PTB: v20

--- NekoRobot/modules/disable.py
+++ NekoRobot/modules/disable.py
@@ -34,14 +34,15 @@ from typing import Union
 
 from future.utils import string_types
 from telegram import Update
-from telegram.constants import ParseMode, ChatType
-from telegram.ext import CommandHandler, MessageHandler, ContextTypes
+from telegram.constants import ChatType, ParseMode
+from telegram.ext import CommandHandler, ContextTypes, MessageHandler
 from telegram.helpers import escape_markdown
+
 from NekoRobot import NEKO_PTB
+from NekoRobot.modules.connection import connected
+from NekoRobot.modules.helper_funcs.alternate import send_message
 from NekoRobot.modules.helper_funcs.handlers import CMD_STARTERS
 from NekoRobot.modules.helper_funcs.misc import is_module_loaded
-from NekoRobot.modules.helper_funcs.alternate import send_message
-from NekoRobot.modules.connection import connected
 
 CMD_STARTERS = tuple(CMD_STARTERS)
 
@@ -49,10 +50,8 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
-    from NekoRobot.modules.helper_funcs.chat_status import (
-        is_user_admin, )
     from NekoRobot.modules.helper_funcs.anonymous import user_admin
-
+    from NekoRobot.modules.helper_funcs.chat_status import is_user_admin
     from NekoRobot.modules.sql import disable_sql as sql
 
     DISABLE_CMDS = []
@@ -60,13 +59,7 @@ if is_module_loaded(FILENAME):
     ADMIN_CMDS = []
 
     class DisableAbleCommandHandler(CommandHandler):
-
-        def __init__(self,
-                     command,
-                     callback,
-                     block=False,
-                     admin_ok=False,
-                     **kwargs):
+        def __init__(self, command, callback, block=False, admin_ok=False, **kwargs):
             super().__init__(command, callback, **kwargs)
             self.admin_ok = admin_ok
             if isinstance(command, string_types):
@@ -86,7 +79,8 @@ if is_module_loaded(FILENAME):
             if message.text and len(message.text) > 1:
                 fst_word = message.text.split(None, 1)[0]
                 if len(fst_word) > 1 and any(
-                        fst_word.startswith(start) for start in CMD_STARTERS):
+                    fst_word.startswith(start) for start in CMD_STARTERS
+                ):
                     args = message.text.split()[1:]
                     command = fst_word[1:].split("@")
                     command.append(message._bot.username)
@@ -101,25 +95,18 @@ if is_module_loaded(FILENAME):
                         chat = update.effective_chat
                         user = update.effective_user
                         # disabled, admincmd, user admin
-                        if sql.is_command_disabled(chat.id,
-                                                   command[0].lower()):
+                        if sql.is_command_disabled(chat.id, command[0].lower()):
                             # check if command was disabled
                             is_ad = asyncio.ensure_future(
-                                is_user_admin(update, user.id))
+                                is_user_admin(update, user.id)
+                            )
                             is_disabled = command[0] in ADMIN_CMDS and is_ad
-                            return (args,
-                                    filter_result) if is_disabled else None
+                            return (args, filter_result) if is_disabled else None
                         return args, filter_result
                     return False
 
     class DisableAbleMessageHandler(MessageHandler):
-
-        def __init__(self,
-                     pattern,
-                     callback,
-                     block=False,
-                     friendly="",
-                     **kwargs):
+        def __init__(self, pattern, callback, block=False, friendly="", **kwargs):
             super().__init__(pattern, callback, **kwargs)
             DISABLE_OTHER.append(friendly or pattern)
             self.friendly = friendly or pattern
@@ -128,21 +115,16 @@ if is_module_loaded(FILENAME):
             if isinstance(update, Update) and update.effective_message:
                 chat = update.effective_chat
                 return self.filters.check_update(
-                    update) and not sql.is_command_disabled(
-                        chat.id, self.friendly)
+                    update
+                ) and not sql.is_command_disabled(chat.id, self.friendly)
 
     @user_admin
-    async def disable(update: Update,
-                      context: ContextTypes.DEFAULT_TYPE) -> None:
+    async def disable(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         chat = update.effective_chat  # type: Optional[Chat]
         user = update.effective_user
         args = context.args
 
-        conn = await connected(context.bot,
-                               update,
-                               chat,
-                               user.id,
-                               need_admin=True)
+        conn = await connected(context.bot, update, chat, user.id, need_admin=True)
         if conn:
             chat = NEKO_PTB.bot.getChat(conn)
             chat_name = NEKO_PTB.bot.getChat(conn).title
@@ -164,7 +146,9 @@ if is_module_loaded(FILENAME):
             if disable_cmd in set(DISABLE_CMDS + DISABLE_OTHER):
                 sql.disable_command(chat.id, disable_cmd)
                 if conn:
-                    text = f"Disabled the use of `{disable_cmd}` command in *{chat_name}*!"
+                    text = (
+                        f"Disabled the use of `{disable_cmd}` command in *{chat_name}*!"
+                    )
                 else:
                     text = f"Disabled the use of `{disable_cmd}` command!"
                 send_message(
@@ -173,27 +157,20 @@ if is_module_loaded(FILENAME):
                     parse_mode=ParseMode.MARKDOWN_V2,
                 )
             else:
-                send_message(update.effective_message,
-                             "This command can't be disabled")
+                send_message(update.effective_message, "This command can't be disabled")
 
         else:
             send_message(update.effective_message, "What should I disable?")
 
     @user_admin
-    async def enable(update: Update,
-                     context: ContextTypes.DEFAULT_TYPE) -> None:
+    async def enable(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         chat = update.effective_chat  # type: Optional[Chat]
         user = update.effective_user
         args = context.args
 
-        conn = await connected(context.bot,
-                               update,
-                               chat,
-                               user.id,
-                               need_admin=True)
+        conn = await connected(context.bot, update, chat, user.id, need_admin=True)
         if conn:
             chat = NEKO_PTB.bot.getChat(conn)
-            chat_id = conn
             chat_name = NEKO_PTB.bot.getChat(conn).title
         else:
             if update.effective_message.chat.type == ChatType.PRIVATE:
@@ -203,7 +180,7 @@ if is_module_loaded(FILENAME):
                 )
                 return ""
             chat = update.effective_chat
-            chat_id = update.effective_chat.id
+            update.effective_chat.id
             chat_name = update.effective_message.chat.title
 
         if len(args) >= 1:
@@ -213,7 +190,9 @@ if is_module_loaded(FILENAME):
 
             if sql.enable_command(chat.id, enable_cmd):
                 if conn:
-                    text = f"Enabled the use of `{enable_cmd}` command in *{chat_name}*!"
+                    text = (
+                        f"Enabled the use of `{enable_cmd}` command in *{chat_name}*!"
+                    )
                 else:
                     text = f"Enabled the use of `{enable_cmd}` command!"
                 send_message(
@@ -222,18 +201,18 @@ if is_module_loaded(FILENAME):
                     parse_mode=ParseMode.MARKDOWN_V2,
                 )
             else:
-                send_message(update.effective_message,
-                             "Is that even disabled?")
+                send_message(update.effective_message, "Is that even disabled?")
 
         else:
             send_message(update.effective_message, "What should I enable?")
 
     @user_admin
-    async def list_cmds(update: Update,
-                        context: ContextTypes.DEFAULT_TYPE) -> None:
+    async def list_cmds(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         if DISABLE_CMDS + DISABLE_OTHER:
-            result = "".join(f" - `{escape_markdown(str(cmd))}`\n"
-                             for cmd in set(DISABLE_CMDS + DISABLE_OTHER))
+            result = "".join(
+                f" - `{escape_markdown(str(cmd))}`\n"
+                for cmd in set(DISABLE_CMDS + DISABLE_OTHER)
+            )
 
             await update.effective_message.reply_text(
                 f"The following commands are toggleable:\n{result}",
@@ -241,8 +220,7 @@ if is_module_loaded(FILENAME):
             )
 
         else:
-            await update.effective_message.reply_text(
-                "No commands can be disabled.")
+            await update.effective_message.reply_text("No commands can be disabled.")
 
     # do not async
     def build_curr_disabled(chat_id: Union[str, int]) -> str:
@@ -253,18 +231,12 @@ if is_module_loaded(FILENAME):
         result = "".join(f" - `{escape_markdown(cmd)}`\n" for cmd in disabled)
         return f"The following commands are currently restricted:\n{result}"
 
-    async def commands(update: Update,
-                       context: ContextTypes.DEFAULT_TYPE) -> None:
+    async def commands(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         chat = update.effective_chat
         user = update.effective_user
-        conn = await connected(context.bot,
-                               update,
-                               chat,
-                               user.id,
-                               need_admin=True)
+        conn = await connected(context.bot, update, chat, user.id, need_admin=True)
         if conn:
             chat = NEKO_PTB.bot.getChat(conn)
-            chat_id = conn
         else:
             if update.effective_message.chat.type == ChatType.PRIVATE:
                 send_message(
@@ -273,12 +245,10 @@ if is_module_loaded(FILENAME):
                 )
                 return ""
             chat = update.effective_chat
-            chat_id = update.effective_chat.id
+            update.effective_chat.id
 
         text = build_curr_disabled(chat.id)
-        send_message(update.effective_message,
-                     text,
-                     parse_mode=ParseMode.MARKDOWN_V2)
+        send_message(update.effective_message, text, parse_mode=ParseMode.MARKDOWN_V2)
 
     def __import_data__(chat_id, data):
         disabled = data.get("disabled", {})
@@ -306,14 +276,18 @@ if is_module_loaded(FILENAME):
   ➛ /listcmds*:* list all possible toggleable commands
     """
 
-    NEKO_PTB.add_handler(CommandHandler(
-        "disable", disable))  # , filters=filters.ChatType.GROUPS)
-    NEKO_PTB.add_handler(CommandHandler(
-        "enable", enable))  # , filters=filters.ChatType.GROUPS)
-    NEKO_PTB.add_handler(CommandHandler(
-        ["cmds", "disabled"], commands))  # , filters=filters.ChatType.GROUPS)
-    NEKO_PTB.add_handler(CommandHandler(
-        "listcmds", list_cmds))  # , filters=filters.ChatType.GROUPS)
+    NEKO_PTB.add_handler(
+        CommandHandler("disable", disable)
+    )  # , filters=filters.ChatType.GROUPS)
+    NEKO_PTB.add_handler(
+        CommandHandler("enable", enable)
+    )  # , filters=filters.ChatType.GROUPS)
+    NEKO_PTB.add_handler(
+        CommandHandler(["cmds", "disabled"], commands)
+    )  # , filters=filters.ChatType.GROUPS)
+    NEKO_PTB.add_handler(
+        CommandHandler("listcmds", list_cmds)
+    )  # , filters=filters.ChatType.GROUPS)
 
 else:
     DisableAbleCommandHandler = CommandHandler
