2d535c18f506b7a8ac7d4887700a5ece5cbcb511
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-08 09:26:31 +0530

    Push PTB: v20.0a2

--- NekoRobot/__init__.py
+++ NekoRobot/__init__.py
@@ -328,23 +328,18 @@ async def get_entity(client, entity):
 
 
 apps = [pgram]
-
 SUDO_USERS = list(SUDO_USERS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
 WHITELIST_USERS = list(WHITELIST_USERS)
 SUPPORT_USERS = list(SUPPORT_USERS)
 TIGERS = list(TIGERS)
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(),
+                                   "NekoRobot/elevated_users.json")
 
 # Load at end to ensure all prev variables have been set
-from NekoRobot.modules.helper_funcs.handlers import (
-    CustomCommandHandler,
-    CustomMessageHandler,
-    CustomRegexHandler,
-)
+from NekoRobot.modules.helper_funcs.handlers import CustomCommandHandler
 
 # make sure the regex handler can take extra kwargs
-tg.RegexHandler = CustomRegexHandler
 tg.CommandHandler = CustomCommandHandler
-tg.MessageHandler = CustomMessageHandler
 
 LOGGER.info("[NEKOROBOT IS READY]")
