d8e0a9e2f067ca4b351e0494d7e41159a6ad9069
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-08 16:57:24 +0530

    Update admin.py

--- NekoRobot/modules/admin.py
+++ NekoRobot/modules/admin.py
@@ -32,7 +32,7 @@ import os
 from telegram import Update
 from telegram.constants import ParseMode
 from telegram.error import BadRequest
-from telegram.ext import CallbackContext, CommandHandler, Filters
+from telegram.ext import CallbackContext, CommandHandler, filters
 from telegram.utils.helpers import mention_html
 from telethon import *
 from telethon import events
