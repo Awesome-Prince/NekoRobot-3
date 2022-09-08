e7a77541837de55c5bf47a43346a1bc9030e52e6
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-08 17:29:01 +0530

    Update sed.py

--- NekoRobot/modules/sed.py
+++ NekoRobot/modules/sed.py
@@ -156,9 +156,7 @@ larger than {}.
 *Reminder:* Sed uses some special characters to make matching easier, such as these: `+*.?\\`
 If you want to use these characters, make sure you escape them!
 *Example:* \\?.
-""".format(
-    telegram.MAX_MESSAGE_LENGTH
-)
+"""
 
 __mod_name__ = "Sed/Regex"
 
