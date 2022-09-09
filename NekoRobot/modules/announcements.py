bba99bc57c4d792af3de085c07385197ad5875ab
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-09-09 13:37:38 +0000

    Push PTB: v20

--- NekoRobot/modules/announcements.py
+++ NekoRobot/modules/announcements.py
@@ -23,21 +23,20 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-import html
 import contextlib
-
+import html
 from typing import Optional
-from telegram.error import TelegramError
-from NekoRobot.modules.helper_funcs.admin_status import A_CACHE, B_CACHE
-from telegram import Update, ChatMemberUpdated
+
+from telegram import ChatMemberUpdated, Update
 from telegram.constants import ParseMode
-from telegram.ext import ContextTypes, ChatMemberHandler
+from telegram.error import TelegramError
+from telegram.ext import ChatMemberHandler, ContextTypes
 
 import NekoRobot.modules.sql.log_channel_sql as logsql
-from NekoRobot import OWNER_ID, NEKO_PTB
-from NekoRobot.modules.log_channel import loggable
-
 import NekoRobot.modules.sql.logger_sql as sql
+from NekoRobot import NEKO_PTB, OWNER_ID
+from NekoRobot.modules.helper_funcs.admin_status import A_CACHE, B_CACHE
+from NekoRobot.modules.log_channel import loggable
 
 # Module to extract and log (optional: send to chat) status changes in chat members using ChatMemberUpdated
 # https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/chatmemberbot.py
@@ -63,29 +62,31 @@ def do_announce(chat):  # announce to chat or only to log channel?
 
 @loggable
 async def chatmemberupdates(
-        update: Update, context: ContextTypes.DEFAULT_TYPE) -> Optional[str]:
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> Optional[str]:
     bot = context.bot
     chat = update.effective_chat
     message = update.effective_message
     log_setting = logsql.get_chat_setting(chat.id)
     if not log_setting:
         logsql.set_chat_setting(
-            logsql.LogChannelSettings(chat.id, True, True, True, True, True))
+            logsql.LogChannelSettings(chat.id, True, True, True, True, True)
+        )
         log_setting = logsql.get_chat_setting(chat.id)
 
     result = extract_status_change(update.chat_member)
     status_change, title_change = result
 
-    if title_change is not None and status_change is None:  # extract title changes for admins
+    if (
+        title_change is not None and status_change is None
+    ):  # extract title changes for admins
         oldtitle, newtitle = title_change
         cause_name = await update.chat_member.from_user.mention_html()
-        member_name = await update.chat_member.new_chat_member.user.mention_html(
-        )
+        member_name = await update.chat_member.new_chat_member.user.mention_html()
         if oldtitle != newtitle:
 
-            if str(update.chat_member.from_user.id) == str(
-                    bot.id):  # bot action
-                return ''  # we handle these in their respective modules
+            if str(update.chat_member.from_user.id) == str(bot.id):  # bot action
+                return ""  # we handle these in their respective modules
 
             if oldtitle is None:
                 if do_announce(chat):
@@ -93,12 +94,14 @@ async def chatmemberupdates(
                         f"{member_name}'s title was set by {cause_name}.\nold title: {oldtitle}\nnew title: '<code>{newtitle}</code>'",
                         parse_mode=ParseMode.HTML,
                     )
-                log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                               f"#ADMIN\nTitle set\n"
-                               f"<b>By Admin:</b> {cause_name}\n"
-                               f"<b>To Admin:</b> {member_name}\n"
-                               f"<b>Old Title:</b> {oldtitle}\n"
-                               f"<b>New Title:</b> '<code>{newtitle}</code>'")
+                log_message = (
+                    f"<b>{html.escape(chat.title)}:</b>\n"
+                    f"#ADMIN\nTitle set\n"
+                    f"<b>By Admin:</b> {cause_name}\n"
+                    f"<b>To Admin:</b> {member_name}\n"
+                    f"<b>Old Title:</b> {oldtitle}\n"
+                    f"<b>New Title:</b> '<code>{newtitle}</code>'"
+                )
                 return log_message
 
             if newtitle is None:
@@ -108,12 +111,14 @@ async def chatmemberupdates(
                         f">'\nnew title: {newtitle}",
                         parse_mode=ParseMode.HTML,
                     )
-                log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                               f"#ADMIN\nTitle removed\n"
-                               f"<b>By Admin:</b> {cause_name}\n"
-                               f"<b>To Admin:</b> {member_name}\n"
-                               f"<b>Old Title:</b> '<code>{oldtitle}</code>'\n"
-                               f"<b>New Title:</b> {newtitle}")
+                log_message = (
+                    f"<b>{html.escape(chat.title)}:</b>\n"
+                    f"#ADMIN\nTitle removed\n"
+                    f"<b>By Admin:</b> {cause_name}\n"
+                    f"<b>To Admin:</b> {member_name}\n"
+                    f"<b>Old Title:</b> '<code>{oldtitle}</code>'\n"
+                    f"<b>New Title:</b> {newtitle}"
+                )
                 return log_message
             if do_announce(chat):
                 await update.effective_chat.send_message(
@@ -121,25 +126,26 @@ async def chatmemberupdates(
                     f">'\nnew title: '<code>{newtitle}</code>'",
                     parse_mode=ParseMode.HTML,
                 )
-            log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                           f"#ADMIN\nTitle changed\n"
-                           f"<b>By Admin:</b> {cause_name}\n"
-                           f"<b>To Admin:</b> {member_name}\n"
-                           f"<b>Old Title:</b> '<code>{oldtitle}</code>'\n"
-                           f"<b>New Title:</b> '<code>{newtitle}</code>'")
+            log_message = (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"#ADMIN\nTitle changed\n"
+                f"<b>By Admin:</b> {cause_name}\n"
+                f"<b>To Admin:</b> {member_name}\n"
+                f"<b>Old Title:</b> '<code>{oldtitle}</code>'\n"
+                f"<b>New Title:</b> '<code>{newtitle}</code>'"
+            )
             return log_message
 
     if status_change is not None:  # exctract chat changes
-        status = ','.join(status_change)
+        status = ",".join(status_change)
         oldstat = str(status.split(",")[0])
         newstat = str(status.split(",")[1])
 
         if str(update.chat_member.from_user.id) == str(bot.id):
-            return ''  # we handle these in their respective modules same as before
+            return ""  # we handle these in their respective modules same as before
 
         cause_name = await update.chat_member.from_user.mention_html()
-        member_name = await update.chat_member.new_chat_member.user.mention_html(
-        )
+        member_name = await update.chat_member.new_chat_member.user.mention_html()
 
         if oldstat == "administrator":
             if newstat == "member":
@@ -152,10 +158,12 @@ async def chatmemberupdates(
                 if not log_setting.log_action:
                     return ""
 
-                log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                               f"#ADMIN\n<b>Demoted</b>\n"
-                               f"<b>Admin:</b> {cause_name}\n"
-                               f"<b>User:</b> {member_name}")
+                log_message = (
+                    f"<b>{html.escape(chat.title)}:</b>\n"
+                    f"#ADMIN\n<b>Demoted</b>\n"
+                    f"<b>Admin:</b> {cause_name}\n"
+                    f"<b>User:</b> {member_name}"
+                )
                 return log_message
 
             if newstat == "kicked":
@@ -168,21 +176,25 @@ async def chatmemberupdates(
                 if not log_setting.log_action:
                     return ""
 
-                log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                               f"#BANNED\n"
-                               f"#ADMIN\n<b>Demoted</b>\n"
-                               f"<b>Admin:</b> {cause_name}\n"
-                               f"<b>User:</b> {member_name}")
+                log_message = (
+                    f"<b>{html.escape(chat.title)}:</b>\n"
+                    f"#BANNED\n"
+                    f"#ADMIN\n<b>Demoted</b>\n"
+                    f"<b>Admin:</b> {cause_name}\n"
+                    f"<b>User:</b> {member_name}"
+                )
                 return log_message
 
             if newstat == "left":
                 if not log_setting.log_action:
                     return ""
 
-                log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                               f"#ADMIN\n<b>Left</b>\n"
-                               f"<b>Admin:</b> {cause_name}\n"
-                               f"<b>User:</b> {member_name}")
+                log_message = (
+                    f"<b>{html.escape(chat.title)}:</b>\n"
+                    f"#ADMIN\n<b>Left</b>\n"
+                    f"<b>Admin:</b> {cause_name}\n"
+                    f"<b>User:</b> {member_name}"
+                )
                 return log_message
 
         if oldstat != "administrator" and newstat == "administrator":
@@ -198,11 +210,13 @@ async def chatmemberupdates(
                     if not log_setting.log_action:
                         return ""
 
-                    log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                                   f"#ADMIN\n<b>Promoted</b>\n"
-                                   f"<b>Admin:</b> {cause_name}\n"
-                                   f"<b>User:</b> {member_name}\n"
-                                   f"<b>Title:</b> '<code>{newtitle}</code>'")
+                    log_message = (
+                        f"<b>{html.escape(chat.title)}:</b>\n"
+                        f"#ADMIN\n<b>Promoted</b>\n"
+                        f"<b>Admin:</b> {cause_name}\n"
+                        f"<b>User:</b> {member_name}\n"
+                        f"<b>Title:</b> '<code>{newtitle}</code>'"
+                    )
                     return log_message
 
             else:
@@ -215,10 +229,12 @@ async def chatmemberupdates(
                 if not log_setting.log_action:
                     return ""
 
-                log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                               f"#ADMIN\n<b>Promoted</b>\n"
-                               f"<b>Admin:</b> {cause_name}\n"
-                               f"<b>User:</b> {member_name}")
+                log_message = (
+                    f"<b>{html.escape(chat.title)}:</b>\n"
+                    f"#ADMIN\n<b>Promoted</b>\n"
+                    f"<b>Admin:</b> {cause_name}\n"
+                    f"<b>User:</b> {member_name}"
+                )
                 return log_message
 
         if oldstat != "restricted" and newstat == "restricted":
@@ -231,10 +247,12 @@ async def chatmemberupdates(
             if not log_setting.log_action:
                 return ""
 
-            log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                           f"#MUTED\n"
-                           f"<b>Admin:</b> {cause_name}\n"
-                           f"<b>User:</b> {member_name}")
+            log_message = (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"#MUTED\n"
+                f"<b>Admin:</b> {cause_name}\n"
+                f"<b>User:</b> {member_name}"
+            )
             return log_message
 
         if oldstat == "restricted" and newstat != "restricted":
@@ -247,10 +265,12 @@ async def chatmemberupdates(
             if not log_setting.log_action:
                 return ""
 
-            log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                           f"#UNMUTED\n"
-                           f"<b>Admin:</b> {cause_name}\n"
-                           f"<b>User:</b> {member_name}")
+            log_message = (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"#UNMUTED\n"
+                f"<b>Admin:</b> {cause_name}\n"
+                f"<b>User:</b> {member_name}"
+            )
             return log_message
 
         if str(update.chat_member.from_user.id) == str(bot.id):
@@ -268,10 +288,12 @@ async def chatmemberupdates(
             if not log_setting.log_action:
                 return ""
 
-            log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                           f"#BANNED\n"
-                           f"<b>Admin:</b> {cause_name}\n"
-                           f"<b>User:</b> {member_name}")
+            log_message = (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"#BANNED\n"
+                f"<b>Admin:</b> {cause_name}\n"
+                f"<b>User:</b> {member_name}"
+            )
             return log_message
 
         if oldstat == "kicked" and newstat != "kicked":
@@ -284,10 +306,12 @@ async def chatmemberupdates(
             if not log_setting.log_action:
                 return ""
 
-            log_message = (f"<b>{html.escape(chat.title)}:</b>\n"
-                           f"#UNBANNED\n"
-                           f"<b>Admin:</b> {cause_name}\n"
-                           f"<b>User:</b> {member_name}")
+            log_message = (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"#UNBANNED\n"
+                f"<b>Admin:</b> {cause_name}\n"
+                f"<b>User:</b> {member_name}"
+            )
             return log_message
 
         if oldstat == ("left" or "kicked") and newstat == "member":
@@ -342,30 +366,32 @@ async def chatmemberupdates(
             return log_message
 
 
-async def mychatmemberupdates(update: Update,
-                              context: ContextTypes.DEFAULT_TYPE) -> None:
+async def mychatmemberupdates(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
     result = extract_status_change(update.my_chat_member)
     status_change, _1 = result
     chat = update.effective_chat
-    chatname = chat.title or chat.first_name or 'None'
-    cause_name = update.effective_user.mention_html(
-    ) if update.effective_user else "Unknown"
+    chatname = chat.title or chat.first_name or "None"
+    cause_name = (
+        update.effective_user.mention_html() if update.effective_user else "Unknown"
+    )
     if status_change is not None:
-        status = ','.join(status_change)
+        status = ",".join(status_change)
         oldstat = str(status.split(",")[0])
         newstat = str(status.split(",")[1])
-        if oldstat == ("left" or "kicked") and newstat == ("member"
-                                                           or "administrator"):
+        if oldstat == ("left" or "kicked") and newstat == ("member" or "administrator"):
             new_group = (
                 f"<b>{html.escape(chat.title) or chat.first_name or chat.id}:</b>\n"
                 f"#NEW_CHAT\n"
                 f"<b>Chat:</b> {chatname}\n"
                 f"<b>Added by:</b> {cause_name or 'none'}\n"
                 f"<b>ID</b>: <code>{update.effective_user.id}</code>\n"
-                f"<b>Chat ID</b>: <code>{update.effective_chat.id}</code>")
-            await context.bot.send_message(OWNER_ID,
-                                           new_group,
-                                           parse_mode=ParseMode.HTML)
+                f"<b>Chat ID</b>: <code>{update.effective_chat.id}</code>"
+            )
+            await context.bot.send_message(
+                OWNER_ID, new_group, parse_mode=ParseMode.HTML
+            )
 
 
 async def admincacheupdates(update: Update):
@@ -374,30 +400,34 @@ async def admincacheupdates(update: Update):
         newstat = update.chat_member.new_chat_member.status
     except AttributeError:
         return
-    if (oldstat == "administrator" and newstat != "administrator"
-            or oldstat != "administrator" and newstat == "administrator"):
+    if (
+        oldstat == "administrator"
+        and newstat != "administrator"
+        or oldstat != "administrator"
+        and newstat == "administrator"
+    ):
 
-        A_CACHE[update.effective_chat.
-                id] = update.effective_chat.get_administrators()
+        A_CACHE[update.effective_chat.id] = update.effective_chat.get_administrators()
         # B_CACHE[update.effective_chat.id] = update.effective_chat.get_member(1241223850)
 
 
 async def botstatchanged(update: Update):
     if update.effective_chat.type != "private":
         with contextlib.suppress(TelegramError):
-            B_CACHE[update.effective_chat.
-                    id] = update.effective_chat.get_member(1241223850)
+            B_CACHE[update.effective_chat.id] = update.effective_chat.get_member(
+                1241223850
+            )
 
 
-NEKO_PTB.add_handler(ChatMemberHandler(chatmemberupdates,
-                                           ChatMemberHandler.CHAT_MEMBER),
-                         group=-21)
-NEKO_PTB.add_handler(ChatMemberHandler(mychatmemberupdates,
-                                           ChatMemberHandler.MY_CHAT_MEMBER),
-                         group=-23)
-NEKO_PTB.add_handler(ChatMemberHandler(botstatchanged,
-                                           ChatMemberHandler.MY_CHAT_MEMBER),
-                         group=-25)
-NEKO_PTB.add_handler(ChatMemberHandler(admincacheupdates,
-                                           ChatMemberHandler.CHAT_MEMBER),
-                         group=-22)
+NEKO_PTB.add_handler(
+    ChatMemberHandler(chatmemberupdates, ChatMemberHandler.CHAT_MEMBER), group=-21
+)
+NEKO_PTB.add_handler(
+    ChatMemberHandler(mychatmemberupdates, ChatMemberHandler.MY_CHAT_MEMBER), group=-23
+)
+NEKO_PTB.add_handler(
+    ChatMemberHandler(botstatchanged, ChatMemberHandler.MY_CHAT_MEMBER), group=-25
+)
+NEKO_PTB.add_handler(
+    ChatMemberHandler(admincacheupdates, ChatMemberHandler.CHAT_MEMBER), group=-22
+)
