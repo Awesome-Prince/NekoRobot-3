cb14cf1d2efbe012d2e207a68a65ea8cb9e5ae07
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-09 21:49:24 +0530

    Update users.py

--- NekoRobot/modules/users.py
+++ NekoRobot/modules/users.py
@@ -1,3 +1,29 @@
+"""
+BSD 2-Clause License
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince ]
+Copyright (c) 2022-2023, Programmer • Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+
 from io import BytesIO
 from time import sleep
 
@@ -168,10 +194,10 @@ BROADCAST_HANDLER = CommandHandler(
     block=False,
 )
 USER_HANDLER = MessageHandler(
-    filters.ALL & filters.ChatType._Groups, log_user, block=False
+    filters.ALL & filters.ChatType.GROUPS, log_user, block=False
 )
 CHAT_CHECKER_HANDLER = MessageHandler(
-    filters.ALL & filters.ChatType._Groups, chat_checker, block=False
+    filters.ALL & filters.ChatType.GROUPS, chat_checker, block=False
 )
 CHATLIST_HANDLER = CommandHandler("groups", chats, block=False)
 
