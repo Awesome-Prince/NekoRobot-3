2ed148bb2afb650e2959586ff5fe09f46ee429c7
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-09 21:48:13 +0530

    Update afk.py

--- NekoRobot/modules/afk.py
+++ NekoRobot/modules/afk.py
@@ -1,8 +1,34 @@
+"""
+BSD 2-Clause License
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince ]
+Copyright (c) 2022-2023, Programmer • Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+
 import random
 
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
-from telegram.ext import CallbackContext, MessageHandler
+from telegram.ext import CallbackContext, MessageHandler, filters
 
 from NekoRobot import NEKO_PTB
 from NekoRobot.modules.disable import (
@@ -141,13 +167,13 @@ async def check_afk(update, context, user_id, fst_name, userc_id):
 
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk, block=False)
 AFK_REGEX_HANDLER = DisableAbleMessageHandler(
-    filter.regex(r"^(?i)brb(.*)$"), afk, friendly="afk"
+    filters.Regex(r"^(?i)brb(.*)$"), afk, friendly="afk"
 )
 NO_AFK_HANDLER = MessageHandler(
-    filter.all & filter.chat_type.groups, no_longer_afk, block=False
+    filters.ALL & filters.ChatType.GROUPS, no_longer_afk, block=False
 )
 AFK_REPLY_HANDLER = MessageHandler(
-    filter.all & filter.chat_type.groups, reply_afk, block=False
+    filters.ALL & filters.ChatType.GROUPS, reply_afk, block=False
 )
 
 NEKO_PTB.add_handler(AFK_HANDLER, AFK_GROUP)
