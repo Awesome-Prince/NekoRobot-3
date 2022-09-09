c7238d3ddd4cb01c6e2d71ec1a072000a77790eb
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-09 21:27:04 +0530

    Update NekoFunBot.py

--- NekoRobot/modules/NekoFunBot.py
+++ NekoRobot/modules/NekoFunBot.py
@@ -1,7 +1,34 @@
+"""
+BSD 2-Clause License
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince ]
+Copyright (c) 2022-2023, Programmer • Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+
 import random
 
 import requests
 
+from telegram.ext import filters
 from NekoRobot import NEKO_PTB
 from NekoRobot.modules.disable import MessageHandler
 
@@ -136,13 +163,13 @@ async def sleep(update, context):
         msg.reply_video(sleepme)
 
 
-KISS_HANDLER = MessageHandler(filter.regex("(?i)Neko kiss"), kiss, block=False)
-BLUSH_HANDLER = MessageHandler(filter.regex("(?i)Neko blush"), blush, block=False)
-PAT_HANDLER = MessageHandler(filter.regex("(?i)Neko pat"), pat, block=False)
-HUG_HANDLER = MessageHandler(filter.regex("(?i)Neko hug"), hug, block=False)
-SLAP_HANDLER = MessageHandler(filter.regex("(?i)Neko slap"), slap, block=False)
-CUTE_HANDLER = MessageHandler(filter.regex("(?i)Neko cute"), cute, block=False)
-SLEEP_HANDLER = MessageHandler(filter.regex("(Neko sleep|sleep)"), sleep, block=False)
+KISS_HANDLER = MessageHandler(filters.Regex("(?i)Neko kiss"), kiss, block=False)
+BLUSH_HANDLER = MessageHandler(filters.Regex("(?i)Neko blush"), blush, block=False)
+PAT_HANDLER = MessageHandler(filters.Regex("(?i)Neko pat"), pat, block=False)
+HUG_HANDLER = MessageHandler(filters.Regex("(?i)Neko hug"), hug, block=False)
+SLAP_HANDLER = MessageHandler(filters.Regex("(?i)Neko slap"), slap, block=False)
+CUTE_HANDLER = MessageHandler(filters.Regex("(?i)Neko cute"), cute, block=False)
+SLEEP_HANDLER = MessageHandler(filters.Regex("(Neko sleep|sleep)"), sleep, block=False)
 
 NEKO_PTB.add_handler(KISS_HANDLER)
 NEKO_PTB.add_handler(PAT_HANDLER)
