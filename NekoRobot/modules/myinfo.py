fdd109a8703660e665b0246b0944a8048c5c1a52
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-09 19:02:45 +0530

    Update myinfo.py

--- NekoRobot/modules/myinfo.py
+++ NekoRobot/modules/myinfo.py
@@ -5,8 +5,7 @@ from datetime import datetime
 
 from telethon import custom, events
 
-from NekoRobot import tbot as bot
-from NekoRobot import tbot as tgbot
+from NekoRobot import tbot
 from NekoRobot.events import register
 
 edit_time = 5
@@ -20,12 +19,12 @@ file5 = "https://telegra.ph/file/737a7b4d519887c35ed21.jpg"
 
 
 @register(pattern="/myinfo")
-async def proboyx(event):
+def proboyx(event):
     await event.get_chat()
     datetime.utcnow()
     betsy = event.sender.first_name
     button = [[custom.Button.inline("Click Here", data="information")]]
-    on = await bot.send_file(
+    on = await tbot.send_file(
         event.chat_id,
         file=file2,
         caption=f"♡ Hey {betsy}, I'm Neko\n♡ I'm Created By [Prince](tg://user?id=1732814103)\n♡ Click The Button Below To Get Your Info",
@@ -33,38 +32,38 @@ async def proboyx(event):
     )
 
     await asyncio.sleep(edit_time)
-    ok = await bot.edit_message(event.chat_id, on, file=file3, buttons=button)
+    ok = await tbot.edit_message(event.chat_id, on, file=file3, buttons=button)
 
     await asyncio.sleep(edit_time)
-    ok2 = await bot.edit_message(event.chat_id, ok, file=file5, buttons=button)
+    ok2 = await tbot.edit_message(event.chat_id, ok, file=file5, buttons=button)
 
     await asyncio.sleep(edit_time)
-    ok3 = await bot.edit_message(event.chat_id, ok2, file=file1, buttons=button)
+    ok3 = await tbot.edit_message(event.chat_id, ok2, file=file1, buttons=button)
 
     await asyncio.sleep(edit_time)
-    ok7 = await bot.edit_message(event.chat_id, ok6, file=file4, buttons=button)
+    ok7 = await tbot.edit_message(event.chat_id, ok6, file=file4, buttons=button)
 
     await asyncio.sleep(edit_time)
-    ok4 = await bot.edit_message(event.chat_id, ok3, file=file2, buttons=button)
+    ok4 = await tbot.edit_message(event.chat_id, ok3, file=file2, buttons=button)
 
     await asyncio.sleep(edit_time)
-    ok5 = await bot.edit_message(event.chat_id, ok4, file=file1, buttons=button)
+    ok5 = await tbot.edit_message(event.chat_id, ok4, file=file1, buttons=button)
 
     await asyncio.sleep(edit_time)
-    ok6 = await bot.edit_message(event.chat_id, ok5, file=file3, buttons=button)
+    ok6 = await tbot.edit_message(event.chat_id, ok5, file=file3, buttons=button)
 
     await asyncio.sleep(edit_time)
-    ok7 = await bot.edit_message(event.chat_id, ok6, file=file5, buttons=button)
+    ok7 = await tbot.edit_message(event.chat_id, ok6, file=file5, buttons=button)
 
     await asyncio.sleep(edit_time)
-    ok7 = await bot.edit_message(event.chat_id, ok6, file=file4, buttons=button)
+    ok7 = await tbot.edit_message(event.chat_id, ok6, file=file4, buttons=button)
 
 
-@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"information")))
+@tbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"information")))
     def callback_query_handler(event):
     try:
         boy = event.sender_id
-        PRO = await bot.get_entity(boy)
+        PRO = await tbot.get_entity(boy)
         NEKO = "YOUR DETAILS BY NEKO \n\n"
         NEKO += f"FIRST NAME : {PRO.first_name} \n"
         NEKO += f"LAST NAME : {PRO.last_name}\n"
