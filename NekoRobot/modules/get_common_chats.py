55ba3f7a857ba7b9e27a4ef521f134f0d11dbb30
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-10 23:13:01 +0530

    Latest Ptb v20

--- NekoRobot/modules/get_common_chats.py
+++ NekoRobot/modules/get_common_chats.py
@@ -24,44 +24,44 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import os
+
 from time import sleep
-
-from telegram import Update
-from telegram.error import BadRequest, Forbidden, RetryAfter
-from telegram.ext import CallbackContext, CommandHandler
-
-from NekoRobot import NEKO_PTB, OWNER_ID
+from NekoRobot import NEKO_PTB
+from NekoRobot.modules.helper_funcs.chat_status import dev_plus
 from NekoRobot.modules.helper_funcs.extraction import extract_user
 from NekoRobot.modules.sql.users_sql import get_user_com_chats
 
+from telegram import Update
+from telegram.constants import ParseMode
+from telegram.error import BadRequest, RetryAfter, Forbidden
+from telegram.ext import ContextTypes, CommandHandler
 
-async def get_user_common_chats(update: Update, context: CallbackContext):
+@dev_plus
+async def get_user_common_chats(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     bot, args = context.bot, context.args
     msg = update.effective_message
     user = extract_user(msg, args)
     if not user:
-        msg.reply_text("I share no common chats with the void.")
+        await msg.reply_text("I share no common chats with the void.")
         return
     common_list = get_user_com_chats(user)
     if not common_list:
-        msg.reply_text("No common chats with this user!")
+        await msg.reply_text("No common chats with this user!")
         return
-    name = bot.get_chat(user).first_name
+    name = await bot.get_chat(user).first_name
     text = f"<b>Common chats with {name}</b>\n"
     for chat in common_list:
         try:
-            chat_name = bot.get_chat(chat).title
-            await sleep(0.3)
-            text += f"• <code>{chat_name}</code>\n"
-        except BadRequest:
-            pass
-        except Forbidden:
+            chat_name = await bot.get_chat(chat).title
+            sleep(0.3)
+            text += f"➛ <code>{chat_name}</code>\n"
+        except (BadRequest, Forbidden):
             pass
         except RetryAfter as e:
-            await sleep(e.retry_after)
+            sleep(e.retry_after)
 
     if len(text) < 4096:
-        msg.reply_text(text, parse_mode="HTML")
+        await msg.reply_text(text, parse_mode=ParseMode.HTML)
     else:
         with open("common_chats.txt", "w") as f:
             f.write(text)
@@ -70,8 +70,4 @@ async def get_user_common_chats(update: Update, context: CallbackContext):
         os.remove("common_chats.txt")
 
 
-COMMON_CHATS_HANDLER = CommandHandler(
-    "getchats", get_user_common_chats, filters=filter.user(OWNER_ID), block=False
-)
-
-NEKO_PTB.add_handler(COMMON_CHATS_HANDLER)
+NEKO_PTB.add_handler(CommandHandler("getchats", get_user_common_chats))
