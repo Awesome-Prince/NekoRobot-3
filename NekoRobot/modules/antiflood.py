021c86ff8b26d0e456bef4174364afb5f3090bff
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-11 09:50:32 +0530

    Update antiflood.py

--- NekoRobot/modules/antiflood.py
+++ NekoRobot/modules/antiflood.py
@@ -2,7 +2,7 @@
 BSD 2-Clause License
 Copyright (C) 2017-2019, Paul Larsen
 Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince]
-Copyright (c) 2022-2023,Programmer Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
+Copyright (c) 2022-2023, Programmer Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
 All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -28,7 +28,7 @@ import html
 import re
 from typing import Optional
 
-from NekooRobot.modules.log_channel import loggable
+from NekoRobot.modules.log_channel import loggable
 from telegram import ChatPermissions, Update
 from telegram.constants import ChatType, ParseMode
 from telegram.error import BadRequest
