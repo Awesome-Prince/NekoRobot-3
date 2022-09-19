d5fbb552fcad24fe7546a0efe8f5fee01e322f69
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-19 19:16:53 +0530

    tg iz banning ub acc...

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -763,13 +763,13 @@ def main():
     else:
         tbot.disconnect()
     updater.idle()
-
-
+"""
 try:
     ubot.start()
 except BaseException:
     print("Userbot Error! Have you added a STRING_SESSION in deploying??")
     sys.exit(1)
+"""
 
 if __name__ == "__main__":
     LOGGER.info(f"Successfully loaded modules: {str(ALL_MODULES)}")
