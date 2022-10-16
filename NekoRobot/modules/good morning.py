e0f383e48fb740018df2ecb73c8d6e9ebfcd877b
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-10-16 14:21:18 +0000

    Auto Code Formatting

--- NekoRobot/modules/good morning.py
+++ NekoRobot/modules/good morning.py
@@ -1,1 +1,40 @@
-"""MIT LicenseCopyright (C) 2017-2019, Paul LarsenCopyright (C) 2022 Awesome-PrinceCopyright (c) 2022, Koy ki   Network, <https://github.com/Awesome-Prince/NekoRobot-3>This file is part of @NekoXRobot (Telegram Bot)Permission is hereby granted, free of charge, to any person obtaining a copyof this software and associated documentation files (the Software), to dealin the Software without restriction, including without limitation the rightsto use, copy, modify, merge, publish, distribute, sublicense, and/or sellcopies of the Software, and to permit persons to whom the Software isfurnished to do so, subject to the following conditions:The above copyright notice and this permission notice shall be included in allcopies or substantial portions of the Software.THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS ORIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THEAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHERLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THESOFTWARE."""from telethon import Buttonfrom NekoRobot import tbot as tbotfrom NekoRobot.events import registerPHOTO = "https://te.legra.ph/file/7a18675abd9b75230735d.mp4"@register(pattern=("Good morning"))async def awake(event):    NEKO = f" Welcome this beautiful morning with a smile on your face. I hope youll have a great day today. Wishing you a very good morning! {event.sender.first_name}"    BUTTON = [        [            Button.url("  UPDATES", "https://telegram.dog/Updates004"),        ]    ]    await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
+"""
+MIT License
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2022 Awesome-Prince
+Copyright (c) 2022, Koy ki   Network, <https://github.com/Awesome-Prince/NekoRobot-3>
+This file is part of @NekoXRobot (Telegram Bot)
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the Software), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from telethon import Button
+
+from NekoRobot import tbot as tbot
+from NekoRobot.events import register
+
+PHOTO = "https://te.legra.ph/file/7a18675abd9b75230735d.mp4"
+
+
+@register(pattern=("Good morning"))
+async def awake(event):
+    NEKO = f" Welcome this beautiful morning with a smile on your face. I hope youll have a great day today. Wishing you a very good morning! {event.sender.first_name}"
+    BUTTON = [
+        [
+            Button.url("  UPDATES", "https://telegram.dog/Updates004"),
+        ]
+    ]
+    await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
