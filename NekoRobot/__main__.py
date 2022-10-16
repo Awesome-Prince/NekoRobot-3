e8f35409f913cdbbda629d07f4edb86b89cbb7ad
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-10-16 16:46:48 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -112,6 +112,14 @@ def get_readable_time(seconds: int) -> str:
 HELP_MSG = "Click The Button Below To Get Help Menu In Your Dm."
 START_MSG = "I'm Awake Already!\n<b>Haven't Slept Since:</b> <code>{}</code>"
 
+NEKO_STICKERS = (
+      "CAACAgUAAxkBAAIIW2NL5v9F9hUom4xmsgNYb63SEfZZAAIZBgACYAF5VIerYoMcSln8KgQ",
+      "CAACAgUAAxkBAAIIT2NL5tVmdAO3n1o3cp9Jic9LkqJvAAK7AgACoU3RVLpdwYfQPmS9KgQ",
+      "CAACAgUAAxkBAAIIUGNL5tVNd0rwdoMv-uDVcZ3hHgwlAAIwAwACP9jQVMv2AfU-DvBfKgQ",
+      "CAACAgUAAxkBAAIIUmNL5tUzCtvS5E-XR8h3tFMSqVu7AAIGAwACNZnYVOTUhlBiHTQGKgQ",
+      "CAACAgUAAxkBAAIIUWNL5tVBEQAB2gWLSq0ymEPeAj5kmQAC3QEAAuu92VSPj3UkCiNvfioE",
+)
+
 PM_START_TEXT = """
 ────「 [{}](https://telegra.ph/file/d80bb89c4aca2a87419b7.jpg) 」────
 *Hola! {},*
@@ -277,6 +285,10 @@ def start(update: Update, context: CallbackContext):
                 IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
 
         else:
+         update.effective_message.reply_sticker(
+                random.choice(NEKO_STICKERS),
+                timeout=60,
+            )
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
                 PM_START_TEXT.format(
