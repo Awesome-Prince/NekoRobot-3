01ebfa867ddeebab68b8cbd1e6b7dc09b1c60d53
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-10-16 14:24:42 +0000

    Auto Code Formatting

--- NekoRobot/modules/helper_funcs/readable_time.py
+++ NekoRobot/modules/helper_funcs/readable_time.py
@@ -23,6 +23,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
+
 def get_readable_time(seconds: int) -> str:
     count = 0
     readable_time = ""
