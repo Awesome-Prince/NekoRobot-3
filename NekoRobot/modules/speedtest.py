ae1b4bac3cb2eda3a1fda419403c76f43701d8be
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2023-05-05 23:07:06 +0600

    Create speedtest.py

---
+++ NekoRobot/modules/speedtest.py
@@ -0,0 +1,93 @@
+"""
+STATUS: Code is working. ✅
+"""
+
+"""
+GNU General Public License v3.0
+
+Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
+
+Credits:-
+    I don't know who originally wrote this code. If you originally wrote this code, please reach out to me. 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import speedtest
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+from telegram.ext import CallbackContext, CallbackQueryHandler
+
+from NekoRobot import DEV_USERS, NEKO_PTB
+from NekoRobot.modules.disable import DisableAbleCommandHandler
+from NekoRobot.modules.helper_funcs.chat_status import dev_plus
+
+
+def convert(speed):
+    return round(int(speed) / 1048576, 2)
+
+
+@dev_plus
+def speedtestxyz(update: Update, context: CallbackContext):
+    buttons = [
+        [
+            InlineKeyboardButton("Image", callback_data="speedtest_image"),
+            InlineKeyboardButton("Text", callback_data="speedtest_text"),
+        ],
+    ]
+    update.effective_message.reply_text(
+        "Select SpeedTest Mode",
+        reply_markup=InlineKeyboardMarkup(buttons),
+    )
+
+
+def speedtestxyz_callback(update: Update, context: CallbackContext):
+    query = update.callback_query
+
+    if query.from_user.id in DEV_USERS:
+        msg = update.effective_message.edit_text("Running a speedtest....")
+        speed = speedtest.Speedtest()
+        speed.get_best_server()
+        speed.download()
+        speed.upload()
+        replymsg = "SpeedTest Results:"
+
+        if query.data == "speedtest_image":
+            speedtest_image = speed.results.share()
+            update.effective_message.reply_photo(
+                photo=speedtest_image,
+                caption=replymsg,
+            )
+            msg.delete()
+
+        elif query.data == "speedtest_text":
+            result = speed.results.dict()
+            replymsg += f"\nDownload: `{convert(result['download'])}Mb/s`\nUpload: `{convert(result['upload'])}Mb/s`\nPing: `{result['ping']}`"
+            update.effective_message.edit_text(replymsg, parse_mode=ParseMode.MARKDOWN)
+    else:
+        query.answer("You are required to join Kingdom Of Science to use this command.")
+
+
+SPEED_TEST_HANDLER = DisableAbleCommandHandler(
+    "speedtest", speedtestxyz, run_async=True
+)
+SPEED_TEST_CALLBACKHANDLER = CallbackQueryHandler(
+    speedtestxyz_callback, pattern="speedtest_.*", run_async=True
+)
+
+NEKO_PTB.add_handler(SPEED_TEST_HANDLER)
+NEKO_PTB.add_handler(SPEED_TEST_CALLBACKHANDLER)
+
+__mod_name__ = "SpeedTest"
+__command_list__ = ["speedtest"]
+__handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
