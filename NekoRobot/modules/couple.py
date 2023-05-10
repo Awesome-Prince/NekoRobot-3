cf5836881292731ad8b32fc647fe36aa8ec2365c
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2023-05-10 19:56:09 +0600

    Update couple.py

--- NekoRobot/modules/couple.py
+++ NekoRobot/modules/couple.py
@@ -1,9 +1,11 @@
 import random
-from NekoRobot import pgram as app
-from NekoRobot.utils.errors import capture_err
-from NekoRobot.modules.mongo.couples_mongo import get_couple, save_couple
-from pyrogram import filters
 from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.enums import ChatType
+
+from NekoRobot import pgram as pbot
+from NekoRobot.modules.mongo.couples_mongo import get_couple, save_couple
 
 
 # Date and time
@@ -29,53 +31,54 @@ today = str(dt()[0])
 tomorrow = str(dt_tom())
 
 
-@app.on_message(filters.command("couples") & ~filters.edited)
-@capture_err
+@pbot.on_message(filters.command(["couple", "couples"]))
 async def couple(_, message):
-    if message.chat.type == "private":
-        await message.reply_text("This command only works in groups.")
-        return
+    if message.chat.type == ChatType.PRIVATE:
+        return await message.reply_text("This command only works in groups.")
     try:
         chat_id = message.chat.id
         is_selected = await get_couple(chat_id, today)
         if not is_selected:
             list_of_users = []
-            async for i in app.iter_chat_members(message.chat.id):
+            async for i in pbot.get_chat_members(message.chat.id, limit=50):
                 if not i.user.is_bot:
                     list_of_users.append(i.user.id)
             if len(list_of_users) < 2:
-                await message.reply_text("Not enough users")
-                return
+                return await message.reply_text("Not enough users")
             c1_id = random.choice(list_of_users)
             c2_id = random.choice(list_of_users)
             while c1_id == c2_id:
                 c1_id = random.choice(list_of_users)
-            c1_mention = (await app.get_users(c1_id)).mention
-            c2_mention = (await app.get_users(c2_id)).mention
+            c1_mention = (await pbot.get_users(c1_id)).mention
+            c2_mention = (await pbot.get_users(c2_id)).mention
 
-            couple_selection_message = f"""**Couple of the day:**
-{c1_mention} + {c2_mention} = ❤️
-__New couple of the day may be chosen at 12AM {tomorrow}__"""
-            await app.send_message(message.chat.id, text=couple_selection_message)
+            couple_selection_message = f"""**Couple of the day :**
+
+{c1_mention} + {c2_mention} = 😘
+__New couple of the day can be chosen at 12AM {tomorrow}__"""
+            await pbot.send_message(message.chat.id, text=couple_selection_message)
             couple = {"c1_id": c1_id, "c2_id": c2_id}
             await save_couple(chat_id, today, couple)
 
         elif is_selected:
             c1_id = int(is_selected["c1_id"])
             c2_id = int(is_selected["c2_id"])
-            c1_name = (await app.get_users(c1_id)).first_name
-            c2_name = (await app.get_users(c2_id)).first_name
-            couple_selection_message = f"""Couple of the day:
-[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❤️
-__New couple of the day may be chosen at 12AM {tomorrow}__"""
-            await app.send_message(message.chat.id, text=couple_selection_message)
+            c1_name = (await pbot.get_users(c1_id)).first_name
+            c2_name = (await pbot.get_users(c2_id)).first_name
+            couple_selection_message = f"""Couple of the day :
+
+[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = 😘
+__New couple of the day can be chosen at 12AM {tomorrow}__"""
+            await pbot.send_message(message.chat.id, text=couple_selection_message)
     except Exception as e:
         print(e)
-        message.reply_text(e)
+        await message.reply_text(e)
 
 
 __help__ = """
- ❍ /couples - To Choose Couple Of The Day
+Choose couples in your chat
 
- """
-__mod_name__ = "Couples"
+ ❍ /couple *:* Choose 2 users and send their name as couples in your chat.
+"""
+
+__mod_name__ = "Cᴏᴜᴘʟᴇ"
