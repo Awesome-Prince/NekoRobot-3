d1f207c29e0133559d0cb17c82b882faf1ec8cc7
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2024-01-30 01:42:42 +0000

    Auto Code Formatting

--- NekoRobot/utils/pastebin.py
+++ NekoRobot/utils/pastebin.py
@@ -23,7 +23,6 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-
 import socket
 from asyncio import get_running_loop
 from functools import partial
